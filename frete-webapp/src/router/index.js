dfe06c522d29daa38421f6ded0f8748f387f94e9
Bossa <l.f.bossa@ufsc.br>
2023-06-12 11:03:07 -0300

    Set Routes and Navigation

---
+++ frete-webapp/src/router/index.js
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/perfil',
+      name: 'perfil',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/items',
+      name: 'items',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ItemsView.vue')
+    }, 
+    {
+      path: '/pacotes',
+      name: 'pacotes',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PacotesView.vue')
+    }, 
+    {
+      path: '/lotes',
+      name: 'lotes',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LotesView.vue')
+    }, 
+    {
+      path: '/qrscan',
+      name: 'qrcode',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ScanearView.vue')
+    }
+  ]
+})
+
+export default router
