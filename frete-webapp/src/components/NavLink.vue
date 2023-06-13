29f94392751ddb8a213ac5de0cd822d30bfe2faa
Bossa <l.f.bossa@ufsc.br>
2023-06-13 18:58:24 -0300

    atualiza a navegação

--- frete-webapp/src/components/NavLink.vue
+++ frete-webapp/src/components/NavLink.vue
@@ -24,11 +24,15 @@ const route = useRoute()
 <style> 
 /* // Small devices (landscape phones, 576px and up) */
 .router-link-active {
-    font-weight: 600;
+    font-weight: 700;
 }
 .navlinkdiv { 
     text-align: center;
 } 
+.router-link-active .navlinkdiv {
+    color: rgb(255, 255, 255); 
+}
+
 @media (max-width: 500px) { 
 .navlinkdiv {
     font-size: 14pt;
