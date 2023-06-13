08cf9d30bdb73f9fedecebf77c1c5a2e588da3b9
Bossa <l.f.bossa@ufsc.br>
2023-06-13 20:42:47 -0300

    ambientes view

--- frete-webapp/src/views/ItemsView.vue
+++ frete-webapp/src/views/ItemsView.vue
@@ -1,7 +1,22 @@
 <script setup>
+import { onBeforeMount } from 'vue';
+import { useAmbientesStore } from '../stores/ambientes';
+
+const ambientes = useAmbientesStore() 
+
 
 </script>
 
 <template> 
 Items
+
+<input type="text" list="ambientesList">
+
+<datalist id="ambientesList">
+    <option v-for="x in ambientes.dados">{{ x.valor }}</option>
+</datalist> 
+
+<button @click="ambientes.load_data">Carrega dados</button>
+
+
 </template>
