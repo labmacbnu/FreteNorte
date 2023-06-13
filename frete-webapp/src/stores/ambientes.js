d9a8cb263a86fd407ce445323b37c04c7b4c0ab1
Bossa <l.f.bossa@ufsc.br>
2023-06-13 20:42:15 -0300

    carrega ambientes

---
+++ frete-webapp/src/stores/ambientes.js
@@ -0,0 +1,20 @@
+import { reactive, ref } from 'vue'
+import { defineStore } from 'pinia'
+import { getFirestore, getDocs,  collection } from 'firebase/firestore'
+import { firebaseApp } from '../firebaseConfig'
+
+
+
+export const useAmbientesStore = defineStore('ambientes', ()=>{ 
+    const db = getFirestore(firebaseApp)
+    const dados = ref([])
+    async function load_data(){
+        const querySnapshot = await getDocs(collection(db, "ambientes"));
+        querySnapshot.forEach( function (doc) { 
+            var docdt = doc.data()
+            docdt.valor = docdt.ambiente_codigo + ' - ' + docdt.ambiente_nome 
+            dados.value.push(docdt) 
+        } )
+    }
+    return {dados, load_data} 
+})
