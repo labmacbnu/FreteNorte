b4c89844d855021ed456d8a6f8a0354284f54042
Bossa <l.f.bossa@ufsc.br>
2023-06-20 09:07:15 -0300

    visualiza os items por código

---
+++ frete-webapp/src/views/ItemsCod.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { getFirestore, getDoc, collection, doc } from 'firebase/firestore'
+import { firebaseApp } from '../firebaseConfig'
+import { onBeforeMount, reactive } from 'vue';
+import { useSingleItemStore } from '../stores/singleitem'
+
+
+const db = getFirestore(firebaseApp)
+
+
+const route = useRoute()
+const codigo = route.params.codigo
+
+const item = useSingleItemStore()
+
+onBeforeMount( async () =>  item.get_item(codigo) )
+
+</script>
+
+<template> 
+<h4>{{item.dados.short_descricao}}</h4>
+ <p>{{item.dados.descricao}}</p>        
+ <p> Edificio: {{ item.dados.edificio }}</p>
+ <p> Ambiente: {{ item.dados.ambiente }}</p>
+ <p> Patrimônio {{ item.dados.patrimonio }}</p>
+ <p> Valor: R$ {{ item.dados.valor }}</p>
+</template>
