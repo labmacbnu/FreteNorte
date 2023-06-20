0b621871e1eed853182f56ae8349d0b45b44ba1a
Bossa <l.f.bossa@ufsc.br>
2023-06-20 09:07:04 -0300

    hack para fixar o items.ambiente

--- frete-webapp/src/views/ItemsAmbiente.vue
+++ frete-webapp/src/views/ItemsAmbiente.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { computed, onBeforeMount, onBeforeUpdate, onUpdated, ref } from 'vue';
+import { computed, onBeforeMount, onBeforeUpdate, onMounted, onUpdated, ref } from 'vue';
 import { useAmbientesStore } from '../stores/ambientes';
 import { useItemsStore } from '../stores/items'
 import AcordeaoChild from '../components/AcordeaoChild.vue';
-import { useRoute } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
 
 const ambientes = useAmbientesStore()
 const items = useItemsStore()
@@ -11,20 +11,24 @@ const route = useRoute()
 
 const ambiente_completo = computed( () => {
     const amb = route.params.ambiente;
-    return ambientes.dados.filter( (elem) => elem.ambiente_codigo == amb )[0]
+    const ambiente = ambientes.dados.filter( (elem) => elem.ambiente_codigo == amb )[0]
+    // hack para atualizar o items store ao calcular o ambiente
+    items.ambiente = ambiente.valor
+    items.load_data()
+    return ambiente
+
 })
  
 
-//onBeforeMount(async () => ambientes.load_data())
-onBeforeUpdate(async () => {  
-    items.load_data()
-})
+//onBeforeMount(async () => ambientes.load_data()) 
+
 
 </script>
 
 <template> 
   
-    <h2>Lista de items de {{ ambiente_completo.ambiente_nome }}</h2> 
+  <RouterLink :to="{name: 'items'}">Voltar</RouterLink>
+    <h4>Lista de items de {{ ambiente_completo.ambiente_nome }}</h4>  
     <div class="accordion" id="acordeao">
         <AcordeaoChild v-for="(valor, chave, n) in items.dados_agrupados" pai="acordeao" :aid="'acord' + n">
             <template #titulo>
@@ -35,6 +39,7 @@ onBeforeUpdate(async () => {
             <ul class="list-group">
                 <li class="list-group-item" v-for="subitem in valor">
                     {{ subitem.descricao }}
+                    <RouterLink :to="{name: 'item-codigo', params: {codigo: subitem.key }}">Ver item</RouterLink>
                 </li>
             </ul>
         </template>
