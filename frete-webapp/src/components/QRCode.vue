27cae7ae3393c4619465e63b2c9be6b7d9f9d3aa
Bossa <l.f.bossa@ufsc.br>
2023-06-21 18:32:52 -0300

    gera qr code

---
+++ frete-webapp/src/components/QRCode.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VueQrcode from 'vue-qrcode';
+const baseurl = "https://frete-norte-ufsc-blumenau.web.app"
+defineProps({
+    path: String
+})
+</script>
+<template>
+<VueQrcode :color="{
+        dark: '#000',
+        light: '#FFF'
+      }" 
+      :value="baseurl + path"
+      :size="500" type="image/png"
+      ></VueQrcode>
+</template>
