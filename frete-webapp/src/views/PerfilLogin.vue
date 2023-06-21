ccabc397bff51c0de6e35d7a68fed47c23970161
Bossa <l.f.bossa@ufsc.br>
2023-06-21 11:19:52 -0300

    Nova página de login

--- frete-webapp/src/views/PerfilLogin.vue
+++ frete-webapp/src/views/PerfilLogin.vue
@@ -1,36 +1,50 @@
-<script setup>
-import { useRoute, useRouter } from "vue-router";
-import { useUserStore } from "../stores/user";
-import {  onBeforeMount, onBeforeUpdate } from "vue"; 
+<script setup> 
+import { loginWithGoogle, globalLogout, useUserStore } from '../stores/user'
+import { useRoute, useRouter } from "vue-router"; 
+import {  computed, inject, onBeforeMount, onBeforeUpdate } from "vue"; 
 
+const user = useUserStore();
+const {globaluser, updateUser} = inject('globaluser') 
 const router = useRouter();
-const user = useUserStore()
-const route = useRoute()
 
-function redirectproperly() { 
-    var next = route.query.redirect
-    if (!next) {
-        var next = { name: 'perfil' }
+function redirectproperly() {
+    const {globaluser, updateUser} = inject('globaluser') 
+    const route = useRoute();
+    const proximo = route.query.redirect 
+    if (globaluser.value) {
+        if(proximo != null) {
+            //console.log(proximo)
+            router.push({path: proximo})
+        } else {
+            console.log("mas que caralho")
+        }
+    } else {
+        console.log('que inferno')
+            //router.push({name: "login"})
     }
-    if (user.isLogged) {
-        router.push(next)
-    }
-
 }
-
-onBeforeMount(() => redirectproperly)
+onBeforeMount(redirectproperly)
 onBeforeUpdate(redirectproperly)
 
 </script>
 
 <template>
- <template v-if="user.isLogged">
-        {{ user.displayName }}
-        <button class="btn btn-primary" @click="user.logout">Logout</button>
+ <template v-if="globaluser">
+    <div class="row">
+    <div class="col">
+         <h2>{{globaluser.displayName}}</h2>
+            <p class="fst-italic"> {{ globaluser.email }} 
+            <span class="badge text-bg-warning">{{ user.role }}</span></p> 
+        </div>
+        <div class="col">
+            <button class="btn btn-primary" @click="globalLogout">Logout</button>
+        </div>
+        </div>
+       
     </template>
     <template v-else>
         O recurso que você está querendo acessar requer que você faça login.
-        <a class="text-decoration-none" @click="user.login('/perfil')">
+        <a class="text-decoration-none" @click="loginWithGoogle">
             <div class="botao-login border border-secondary mx-auto">
                 <img class="imagem-login"
                     src="https://developers.google.com/static/identity/images/g-logo.png?hl=pt-br">Fazer login com Google
