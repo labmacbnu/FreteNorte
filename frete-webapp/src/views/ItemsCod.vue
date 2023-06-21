087c19bc506138130d8976eea3c3cc672bb32922
Bossa <l.f.bossa@ufsc.br>
2023-06-21 18:33:38 -0300

    lista as propriedades do item

--- frete-webapp/src/views/ItemsCod.vue
+++ frete-webapp/src/views/ItemsCod.vue
@@ -2,9 +2,9 @@
 import { useRoute } from 'vue-router';
 import { getFirestore, getDoc, collection, doc } from 'firebase/firestore'
 import { firebaseApp } from '../firebaseConfig'
-import { onBeforeMount, reactive } from 'vue';
+import { onBeforeMount, onMounted } from 'vue';
 import { useSingleItemStore } from '../stores/singleitem'
-
+import QRCode from '../components/QRCode.vue';
 
 const db = getFirestore(firebaseApp)
 
@@ -14,15 +14,49 @@ const codigo = route.params.codigo
 
 const item = useSingleItemStore()
 
-onBeforeMount( async () =>  item.get_item(codigo) )
+
+onBeforeMount(async () => await item.get_item(codigo))
 
 </script>
 
-<template> 
-<h4>{{item.dados.short_descricao}}</h4>
- <p>{{item.dados.descricao}}</p>        
- <p> Edificio: {{ item.dados.edificio }}</p>
- <p> Ambiente: {{ item.dados.ambiente }}</p>
- <p> Patrimônio {{ item.dados.patrimonio }}</p>
- <p> Valor: R$ {{ item.dados.valor }}</p>
+<template>
+    <h4 class="d-print-none">{{ item.short_descricao }}</h4>
+
+    <table class="table d-print-none">
+        <tbody>
+            <tr>
+                <td colspan="2">{{ item.descricao }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Ambiente</th>
+                <td>{{ item.ambiente }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Patrimônio</th>
+                <td>{{ item.patrimonio }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Valor</th>
+                <td>{{ item.valor }}</td>
+            </tr>
+
+            <tr>
+                <th scope="row">Medidas</th>
+                <td><input class="form-control" v-model="item.medidas" disabled></td>
+            </tr>
+
+            <tr>
+                <th scope="row">Peso</th>
+                <td><input class="form-control" v-model="item.peso" disabled></td>
+            </tr>
+        </tbody>
+    </table>
+    <h3 class="d-none d-print-block">Sistema Frete Norte</h3>
+    <div class="float-start">
+        <QRCode :path="route.fullPath"></QRCode>
+    </div>
+    <div class="float-start">
+        <h5>{{ item.short_descricao }}</h5>
+        <p><em>Patrimônio</em> {{ item.patrimonio }}</p>
+    </div>
 </template>
