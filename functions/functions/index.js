c142448d50659d2e7e236f8b98ecea5c2cce905c
Bossa <l.f.bossa@ufsc.br>
2023-06-23 15:19:05 -0300

    Função de registrar permissões
    Na primeira vez que o usuário loga, cria um registro de permissões

---
+++ functions/functions/index.js
@@ -0,0 +1,31 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+ 
+const logger = require("firebase-functions/logger");
+ 
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   resp 
+const  functions  = require("firebase-functions");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore")
+
+initializeApp();
+const db = getFirestore();
+
+exports.registrausuario = functions.auth.user().onCreate( (user, context) => {
+    const email = user.email;
+    const nome = user.displayName;
+    db.doc(`permissoes/${email}`).set({role: "Usuário", ambientes: [], nome: nome})
+    logger.log(`Usuário criado: ${email}`)
+
+})
