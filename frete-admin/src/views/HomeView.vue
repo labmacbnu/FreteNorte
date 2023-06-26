7b526b2e225343e277eab5dabc2eeed1c7d8a5ea
Bossa <l.f.bossa@ufsc.br>
2023-06-26 12:44:53 -0300

    lint

--- frete-admin/src/views/HomeView.vue
+++ frete-admin/src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <script setup>
 </script>
 
-<template> 
+<template>
     <div class="row my-3">
         <div class="col">
-    <h1>SisLog Admin</h1>
-    <p>Sistema de controle da logística da mudança do campus Blumenau.</p>
-    <p>Para acessar o sistema, você precisará usar o login pelo Google usando uma conta idUFSC.</p>
+            <h1>SisLog Admin</h1>
+            <p>Sistema de controle da logística da mudança do campus Blumenau.</p>
+            <p>Para acessar o sistema, você precisará usar o login pelo Google usando uma conta idUFSC.</p> 
+        </div>
     </div>
-    </div>
-    
 </template>
