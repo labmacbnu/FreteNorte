c0d1be5ca0e8fe2336ec1c033066089bc2e7b05e
Bossa <l.f.bossa@ufsc.br>
2023-06-26 12:45:07 -0300

    add vuetify

--- frete-admin/src/main.js
+++ frete-admin/src/main.js
@@ -1,16 +1,13 @@
 import { createApp, ref } from 'vue'
-import { createPinia } from 'pinia' 
-
+import { createPinia } from 'pinia'  
 import App from './App.vue'
-import router from './router' 
+import router from './router'  
 
 const app = createApp(App)
-
-
+ 
 
 
 app.use(createPinia())
-app.use(router) 
-
+app.use(router)  
 
 app.mount('#app')
