cbd2893f9af006cebc5cfef6a54b1f0e9447f236
Bossa <l.f.bossa@ufsc.br>
2023-06-26 12:42:38 -0300

    edita user roles

---
+++ frete-admin/src/components/UserRole.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+defineEmits(["updaterole"])
+defineProps({
+    role: String,
+    label: String
+})
+
+const roles = ref(["Usuário", "Administrador", "Líder de Ambiente", "Comissão de Logística"])
+
+</script>
+<template> 
+<div class="form-floating">
+  <select class="form-select" :value="role" id="floatingSelect" aria-label="Floating label select example">
+    <option v-for="x in roles">{{ x }}</option>    
+  </select>
+  <label for="floatingSelect">{{label}}</label>
+</div>
+
+</template>
