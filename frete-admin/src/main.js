e98cdd7f57556c7afdb3756981f90bf6eafebfef
Bossa <l.f.bossa@ufsc.br>
2023-06-28 17:42:37 -0300

    npm install vuefire

--- frete-admin/src/main.js
+++ frete-admin/src/main.js
@@ -2,6 +2,8 @@ import { createApp, ref } from 'vue'
 import { createPinia } from 'pinia'  
 import App from './App.vue'
 import router from './router'  
+import { VueFire } from 'vuefire';
+import { firebaseApp } from './firebaseConfig';
 
 const app = createApp(App)
  
@@ -9,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)  
-
+app.use(VueFire, {firebaseApp})
 app.mount('#app')
