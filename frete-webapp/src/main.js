5e1a84d21d54c5cec390eb519e4674b5803d1e90
Bossa <l.f.bossa@ufsc.br>
2023-06-29 10:51:56 -0300

    Add VueFire

--- frete-webapp/src/main.js
+++ frete-webapp/src/main.js
@@ -2,7 +2,7 @@ import { createApp, ref } from 'vue'
 import { createPinia } from 'pinia'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { firebaseApp } from './firebaseConfig'
-
+import {VueFire} from 'vuefire'
 import App from './App.vue'
 import router from './router' 
 
@@ -13,6 +13,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router) 
+app.use(VueFire, {firebaseApp})
 
 
 app.mount('#app')
