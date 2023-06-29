48708463674fd16f9e6fcd0ec2d52a504a79b470
Bossa <l.f.bossa@ufsc.br>
2023-06-29 10:46:49 -0300

    Função atualiza item

--- frete-webapp/src/views/ItemsCod.vue
+++ frete-webapp/src/views/ItemsCod.vue
@@ -1,31 +1,70 @@
 <script setup>
 import { useRoute } from 'vue-router';
-import { getFirestore, getDoc, collection, doc } from 'firebase/firestore'
+import { getFirestore, doc } from 'firebase/firestore'
 import { firebaseApp } from '../firebaseConfig'
-import { onBeforeMount, onMounted } from 'vue';
-import { useSingleItemStore } from '../stores/singleitem'
+import { computed, reactive } from 'vue';
+import { update_item } from '../stores/singleitem'
 import QRCode from '../components/QRCode.vue';
+import { useDocument } from 'vuefire';
 
 const db = getFirestore(firebaseApp)
+const route = useRoute()  
+const {
+  // rename the Ref to something more meaningful
+  data: item_db,
+  // is the subscription still pending?
+  pending,
+  // did the subscription fail?
+  error,
+  // A promise that resolves or rejects when the initial state is loaded
+  promise
+} = useDocument(doc(db, 'items', route.params.codigo))
 
+const item = computed(() => {
+    if(item_db.value) {
+        return {
+            ...item_db.value,
+            key: item_db.value.id
+        }
+    } else {
+        return {
+            key: "",
+            descricao: "",
+            short_descricao: "",
+            ambiente: "",
+            patrimonio: "",
+            valor: "",
+            medidas: "",
+            peso: "",
+            fragil: false,
+            transporte_especial: false
+        }
+    }
+})
 
-const route = useRoute()
-const codigo = route.params.codigo
+async function atualiza_item() {   
+    const valores = {...item.value}
+    console.log(valores) 
+    const uptime = await update_item(valores.key, { 
+        medidas: valores.medidas, 
+        peso: valores.peso? valores.peso : null,
+        fragil: valores.fragil? valores.fragil : null,
+        transporte_especial: valores.transporte_especial? valores.transporte_especial : null
+    })
 
-const item = useSingleItemStore()
-
-
-onBeforeMount(async () => await item.get_item(codigo))
-
+    console.log(`Item ${valores.key} atualizado ${uptime}`)
+}
 </script>
 
-<template>
-    <h4 class="d-print-none">{{ item.short_descricao }}</h4>
+<template> 
+    <div class="row align-items-start">
+        <div class="col-8 col-lg-6">
+            <h4 class="d-print-none">{{ item.short_descricao }}</h4>
 
     <table class="table d-print-none">
         <tbody>
             <tr>
-                <td colspan="2">{{ item.descricao }}</td>
+                <td colspan="2" class="text-secondary">{{ item.descricao }}</td>
             </tr>
             <tr>
                 <th scope="row">Ambiente</th>
@@ -42,21 +81,51 @@ onBeforeMount(async () => await item.get_item(codigo))
 
             <tr>
                 <th scope="row">Medidas</th>
-                <td><input class="form-control" v-model="item.medidas" disabled></td>
+                <td><input class="form-control" v-model="item.medidas"></td>
             </tr>
 
             <tr>
                 <th scope="row">Peso</th>
-                <td><input class="form-control" v-model="item.peso" disabled></td>
+                <td><input class="form-control" v-model="item.peso"></td>
+            </tr>
+
+            <tr>
+                <th scope="row">Extras</th>
+                <td><div class="form-check">
+  <input class="form-check-input" type="checkbox" v-model="item.fragil" id="checkfragil">
+  <label class="form-check-label" for="checkfragil">
+    Frágil
+  </label>
+</div><div class="form-check">
+  <input class="form-check-input" type="checkbox"  v-model="item.transporte_especial" id="checkespecial">
+  <label class="form-check-label" for="checkespecial">
+    Transporte especial
+  </label>
+</div>
+</td>
+            </tr>
+            <tr class="border-primary">
+                <td colspan="2" class="text-end d-print-none">
+                    <button class="btn btn-primary" @click="atualiza_item">Salvar</button>
+                </td>
             </tr>
         </tbody>
     </table>
-    <h3 class="d-none d-print-block">Sistema Frete Norte</h3>
-    <div class="float-start">
-        <QRCode :path="route.fullPath"></QRCode>
+    
+        </div>
     </div>
-    <div class="float-start">
-        <h5>{{ item.short_descricao }}</h5>
-        <p><em>Patrimônio</em> {{ item.patrimonio }}</p>
-    </div>
+    <div class="row align-items-start">
+        <div class="col-3">
+            <QRCode :path="route.fullPath"></QRCode>
+        </div>
+        <div class="col-6">
+            <h4 class="d-none d-print-block">Sistema Frete Norte</h4> 
+            <div class="float-start">
+                <h5>{{ item.short_descricao }}</h5>
+                <p><small>
+                     {{ item.ambiente }}</small></p>
+                <p><em>Patrimônio</em> {{ item.patrimonio }}</p>
+            </div>
+        </div>
+    </div> 
 </template>
