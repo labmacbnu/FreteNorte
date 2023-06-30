f717e177da2455bcab612b69b14da119345cdfdf
Bossa <l.f.bossa@ufsc.br>
2023-06-30 17:40:15 -0300

    Pega um pacote pelo código

---
+++ frete-webapp/src/views/PacotesCodigo.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useRoute, RouterLink } from 'vue-router'
+import { doc } from 'firebase/firestore'
+import { useDocument } from 'vuefire'
+import { db, getdoc } from '../backend/index'
+import { onBeforeMount, ref } from 'vue'
+
+const route = useRoute()
+const codigo = route.params.codigo
+const {
+  // rename the Ref to something more meaningful
+  data: volume,
+  // is the subscription still pending?
+  pending,
+  // did the subscription fail?
+  error,
+  // A promise that resolves or rejects when the initial state is loaded
+  promise
+} = useDocument(doc(db, "volumes", codigo))
+
+const responsavel = ref(null)
+ 
+</script>
+<template>
+    <h1>Volume {{ volume.codigo }}</h1>
+{{ volume.responsavel.nome }}
+</template>
