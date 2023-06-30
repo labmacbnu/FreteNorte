3470fc30da69ae145efa03e18d5dcb4077af1ec5
Bossa <l.f.bossa@ufsc.br>
2023-06-30 17:40:38 -0300

    função softdelete volume

--- frete-webapp/src/stores/volumes.js
+++ frete-webapp/src/stores/volumes.js
@@ -1,7 +1,7 @@
 import { computed, ref } from 'vue';
 import { defineStore } from 'pinia';
-import {firebaseApp} from '../firebaseConfig';
-import {  collection, where, doc, setDoc, query } from 'firebase/firestore';
+import { firebaseApp } from '../firebaseConfig';
+import {  collection, where, doc, setDoc, query, updateDoc } from 'firebase/firestore';
 import {useCollection, useDocument} from 'vuefire';
 import { db } from '../backend/index.js';
  
@@ -15,7 +15,7 @@ export const useNumVolumesStore = defineStore("volumes-num",  () => {
         } else {
             const quantidade = volumes.value.quantidade
             const base36 = quantidade.toString(36).toUpperCase()
-            const padded = base36.padStart(3, "0")
+            const padded = base36.padStart(4, "0")
         return      "V" + padded
         }
     })
@@ -23,12 +23,21 @@ export const useNumVolumesStore = defineStore("volumes-num",  () => {
 })
 
 export async function registra_volume(dados){
+    dados.responsavel = doc(db, "permissoes", dados.responsavel)
     const docRef = doc(db, "volumes", dados.codigo);
-    const uptime = await setDoc(docRef, dados);
+    const uptime = await setDoc(docRef, {...dados, deleted: false});
 }
 
+
+export async function apaga_volume(codigo_volume){ 
+    const docRef = doc(db, "volumes", codigo_volume);
+    const uptime = await updateDoc(docRef, {deleted: true});
+}
+
+
 export function lista_volumes(email){ 
+    const userRef = doc(db, "permissoes", email)
     const volRef = collection(db, "volumes")    
-    const q = query(volRef, where("responsavel", "==", email))
+    const q = query(volRef, where("responsavel", "==", userRef), where('deleted', '==', false))
     return useCollection(q)  
 }
