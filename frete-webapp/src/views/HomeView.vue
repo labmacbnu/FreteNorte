15d0135417e29e0aebf3dd5db4af5a395254819f
Bossa <l.f.bossa@ufsc.br>
2023-07-07 18:14:00 -0300

    Adiciona uma visualização dos ambientes na Home
    Mostra uma lista de ambientes e uma barra de progresso

--- frete-webapp/src/views/HomeView.vue
+++ frete-webapp/src/views/HomeView.vue
@@ -1,13 +1,58 @@
 <script setup>
+import { useAmbientesStore, ambiente_status } from '../stores/ambientes';
+import { useUserPermissionsStore } from '../stores/user';
+import { RouterLink } from 'vue-router';
+import { computed, ref, reactive, onMounted } from 'vue';
+import { list } from 'firebase/storage';
 
+
+
+const ambientes = useAmbientesStore()
+const permissions = useUserPermissionsStore()
+
+
+const meus_ambientes = computed(() => {
+
+    const lista_ambientes = reactive([])
+    if (permissions.ambientes) {
+        lista_ambientes.push(...ambientes.dados.filter(obj => permissions.ambientes.includes(obj.ambiente_codigo)))
+
+        lista_ambientes.forEach(async (ambiente, index) => {
+            const dicio = await ambiente_status(ambiente.valor)
+            dicio.percent = parseInt(100* dicio.volumados / dicio.todos)
+            lista_ambientes[index]["status"] = reactive(dicio)
+        })
+    }
+    return lista_ambientes
+}) 
 </script>
 
-<template> 
+<template>
     <div class="row my-3">
         <div class="col">
-    <h1>SisLog</h1>
-    <p>Sistema de controle da logística da mudança do campus Blumenau.</p>
-    <p>Para acessar o sistema, você precisará usar o login pelo Google usando uma conta idUFSC.</p>
-    </div>
+            <h1>SisLog Norte</h1>
+            <p> Sistema de controle da logística da mudança do campus Blumenau.</p>
+            <h2>Seus ambientes</h2>
+            <div class="my-3">
+                <div class="card" v-for="(x, i) in meus_ambientes">
+                    <div class="card-body" v-if="x">
+                        <h5 class="card-title">{{ x.ambiente_codigo }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ x.ambiente_nome }}</h6>
+                        <p class="card-text">
+                        <div v-if="x.status" class="progress" role="progressbar" aria-label="Default striped example"
+                            :aria-valuenow="x.status.volumados" aria-valuemin="0" :aria-valuemax="x.status.todos">
+                            <div class="progress-bar progress-bar-striped" :style="{ width: x.status.percent + '%' }">
+                                {{ x.status.volumados }} de {{ x.status.todos }} volumados</div>
+                        </div>
+                        </p>
+                        <RouterLink :to="{ name: 'items-ambiente', params: { ambiente: x.ambiente_codigo } }">Ver ambiente
+                        </RouterLink>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
     </div>
 </template>
