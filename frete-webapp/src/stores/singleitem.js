780694bb770c6722df1a8b3e84e10204722eea68
Bossa <l.f.bossa@ufsc.br>
2023-07-07 11:58:33 -0300

    Funções para trabalhar com as partes
    - `uptade_item_part` adiciona ou remove uma parte à um item
    - `create_part` cria uma parte
    - `get_parte_ref` retorna uma referencia à parte no firestore
    - `delete_part` hard deleta uma parte do firestore

--- frete-webapp/src/stores/singleitem.js
+++ frete-webapp/src/stores/singleitem.js
@@ -1,8 +1,8 @@
-import { getFirestore, getDoc, updateDoc, doc } from 'firebase/firestore'
+import { getFirestore, getDoc, setDoc, updateDoc, doc, arrayUnion, arrayRemove, deleteDoc } from 'firebase/firestore'
 import { firebaseApp } from '../firebaseConfig'
 import { defineStore } from 'pinia' 
 import { useDocument } from  'vuefire';
-import { ref, computed } from 'vue';
+import { ref, computed } from 'vue'; 
 
 export const useSingleItemStoreOld = defineStore("single-item-old", {  
     state: () => ({
@@ -43,4 +43,39 @@ export async function update_item (codigo, item_data){
     const docRef = doc(db, "items", codigo)
     const uptime = await updateDoc(docRef, {...item_data})
     return uptime
+}
+
+export async function update_item_part(item_codigo, operation, partRef){
+    const db = getFirestore(firebaseApp) 
+    const docRef = doc(db, "items", item_codigo)
+    if(operation == "add"){
+        const uptime = await updateDoc(docRef, 
+            {partes: arrayUnion(partRef)}
+        ) 
+        return true
+    } else if (operation == "remove"){
+        const uptime = await updateDoc(docRef, 
+            {partes: arrayRemove(partRef)}
+        ) 
+        return true
+    } else {
+        return false
+    }
+}
+
+export async function create_part(part_data){
+    const db = getFirestore(firebaseApp) 
+    const partRef = doc(db, "items-partes", part_data.key)
+
+    const uptime = await setDoc(partRef, {...part_data})
+    return partRef
+}
+
+export async function get_parte_ref(part_key){
+    const db = getFirestore(firebaseApp) 
+    return doc(db, "items-partes", part_key)
+}
+
+export async function delete_part(part_key){
+    await deleteDoc(get_parte_ref(part_key)) 
 }
