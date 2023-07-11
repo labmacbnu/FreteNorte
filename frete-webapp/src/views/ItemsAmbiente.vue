3f476736385fdbfab60db58ed1f4e7bec92f865c
Bossa <l.f.bossa@ufsc.br>
2023-07-11 10:55:42 -0300

    Adiciona link para ItemsAdd
    redireciona para a página de adicionar items

--- frete-webapp/src/views/ItemsAmbiente.vue
+++ frete-webapp/src/views/ItemsAmbiente.vue
@@ -24,7 +24,9 @@ const ambiente_completo = computed( () => {
   <div class="row justify-contents-start">
     <div class="col-12"> 
      <p class="m-1"><RouterLink :to="{name: 'items'}"><i class="bi bi-arrow-left-short"></i>Voltar</RouterLink></p>
-      
+     <p class="m-3 text-end">
+      <RouterLink class="btn btn-primary" :to="{name: 'item-add', query: {ambiente: ambiente_completo.ambiente_codigo}}"><i class="bi bi-plus-circle me-2"></i>Adicionar item</RouterLink>
+     </p>
     <h4>Lista de items de {{ ambiente_completo.ambiente_nome }}</h4> 
     <Acordeao id="acordeao" :lista_agrupada="items.dados_agrupados" />
     </div>
