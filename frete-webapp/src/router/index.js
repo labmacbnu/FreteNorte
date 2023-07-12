7b43bbd285698de1608927994708f1b56d2a3377
Bossa <l.f.bossa@ufsc.br>
2023-07-12 19:12:01 -0300

    Elimina a página Lotes

--- frete-webapp/src/router/index.js
+++ frete-webapp/src/router/index.js
@@ -100,17 +100,6 @@ const router = createRouter({
       ]
     },
     {
-      path: '/lotes',
-      name: 'lotes',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LotesView.vue'),
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
       path: '/qrscan',
       name: 'qrcode',
       // route level code-splitting
