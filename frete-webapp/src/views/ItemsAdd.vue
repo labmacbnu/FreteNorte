544342b9985bae4adcdbe17cb9626e79382e5e6c
Bossa <l.f.bossa@ufsc.br>
2023-07-12 19:11:11 -0300

    Adiciona items
    Adiciona items e com validação

--- frete-webapp/src/views/ItemsAdd.vue
+++ frete-webapp/src/views/ItemsAdd.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { onBeforeMount, reactive, computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { onBeforeMount, reactive, computed, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import { useUserPermissionsStore } from '../stores/user';
 import { useAmbientesStore } from '../stores/ambientes';
+import { cria_item } from '../stores/items';
 
 const route = useRoute();
+const router = useRouter();
 const ambientes = useAmbientesStore();
 const permissions = useUserPermissionsStore();
 
@@ -16,6 +18,7 @@ const itemModel = reactive({
     key: "",
     descricao: "",
     short_descricao: "",
+    categoria: "consumível",
     ambiente: "",
     medidas: null,
     peso: null,
@@ -25,6 +28,39 @@ const itemModel = reactive({
     observacoes: "",
     volume: null,
 })
+
+const valido = reactive({
+    descricao: true,
+    ambiente: true,
+})
+
+const goBack = function(){
+    if (route.query.ambiente) {
+        router.push({name: 'items-ambiente', params: {ambiente: route.query.ambiente}})
+    } else {
+        router.push({name: 'items'})
+    }
+}
+
+async function saveItem() {
+    valido.descricao = itemModel.descricao.length > 0
+    valido.ambiente = itemModel.ambiente != ""
+    if(valido.descricao && valido.ambiente) {
+        const item = {...itemModel} 
+        item.short_descricao = item.descricao
+        console.log(item)
+        const id = await cria_item(item)
+        if (id) {
+            router.push({name: 'item-codigo', params: {codigo: id}})
+        }
+    } else {
+        console.log("invalido")
+        setTimeout( () => {
+            valido.descricao = true
+            valido.ambiente = true
+        }, 2500)
+    }
+}
 
 onBeforeMount(() => {
     if (route.query.ambiente) {
@@ -36,15 +72,15 @@ onBeforeMount(() => {
     <div class="row">
         <div class="col">
             <h1>Adicionar item</h1> 
-            <p>Items adicionados aqui são items que não tem número de patrimônio, mas que você quer
-                manter o controle durante o processo de mudança.</p>
-            <form>
+            <p class="form-text">Items adicionados aqui são items que não tem número de patrimônio, mas que você quer
+                manter o controle durante o processo de mudança.</p> 
+            
                 <div class="row g-3 mb-2 justify-items-start">
-                    <label for="ambiente" class="col-lg-1 col-sm-2 col-form-label me-3 me-3">Ambiente</label>
-                    <div class="col-auto col-lg-8">
-                        <select v-model="itemModel.ambiente" class="form-select" aria-label="Ambiente">
+                    <label for="ambiente" class="col-lg-1 col-sm-2 col-form-label me-3">Ambiente</label>
+                    <div class="col-auto">
+                        <select v-model="itemModel.ambiente" class="form-select" :class="{'border-danger': !valido.ambiente}" aria-label="Ambiente">
                             <option v-for="ambiente in meus_ambientes" :value="ambiente.ambiente_codigo">
-                                {{ ambiente.ambiente_nome }}
+                                {{ ambiente.valor }}
                             </option>
                             </select>
 
@@ -54,21 +90,21 @@ onBeforeMount(() => {
                 <div class="row g-3 mb-2 justify-items-start">
                     <label for="descricao" class="col-lg-1 col-sm-2 col-form-label me-3">Descrição</label>
                     <div class="col-auto col-lg-8">
-                        <input type="text" class="form-control" id="descricao" v-model="itemModel.descricao">
+                        <input :class="{'border-danger': !valido.descricao}" placeholder="e.g., Reagentes, Vidraria, Placas de Circuito, Tecidos" type="text" class="form-control" id="descricao" v-model="itemModel.descricao">
                     </div>
                 </div>
 
                 <div class="row g-3 mb-2 justify-items-start">
                     <label for="medidas" class="col-lg-1 col-sm-2 col-form-label me-3 ">Medidas</label>
                     <div class="col-auto col-lg-8">
-                        <input type="text" class="form-control" id="medidas" v-model="itemModel.medidas">
+                        <input placeholder="L x A x P cm" type="text" class="form-control" id="medidas" v-model="itemModel.medidas">
                     </div>
                 </div>
 
                 <div class="row g-3 mb-2 justify-items-start">
                     <label for="peso" class="col-lg-1 col-sm-2 col-form-label me-3 ">Peso</label>
                     <div class="col-auto col-lg-8">
-                        <input type="text" class="form-control" id="peso" v-model="itemModel.peso">
+                        <input placeholder="xyz (g|kg)" type="text" class="form-control" id="peso" v-model="itemModel.peso">
                     </div>
                 </div>
                 <div class="row g-3 mb-2 justify-items-start">
@@ -88,19 +124,24 @@ onBeforeMount(() => {
                 <div class="row g-3 mb-2 justify-items-start">
                     <label for="observacoes" class="col-lg-1 col-sm-2 col-form-label me-3 ">Observações</label>
                     <div class="col-auto col-lg-8">
-                        <input type="text" class="form-control" id="observacoes" v-model="itemModel.observacoes">
+                        <input placeholder="Precisa de calibração ao instalar? Precisa de algum cuidado a mais no transporte?" type="text" class="form-control" id="observacoes" v-model="itemModel.observacoes">
                     </div>
                 </div>
 
-                <div class="row g-3 mb-2 justify-items-end">
-                    <div class="col-7 col-lg-8"></div>
+                <div class="row g-3 m-2 justify-items-end">
+                    <div class="col col-lg-8"></div>
                     <div class="col-auto">
-                        <button class="btn btn-primary">
+                        <button class="btn btn-secondary" 
+                        @click="goBack">
+                            Cancelar
+                            </button>
+                    </div>
+                    <div class="col-auto">
+                        <button @click="saveItem" class="btn btn-primary">
                             Salvar
                             </button>
                     </div>
                     </div>
-            </form>
         </div>
     </div>
 </template>
