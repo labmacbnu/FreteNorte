4a94aa59035cc9a0cd2a011c87960d70a90b07c7
Bossa <l.f.bossa@ufsc.br>
2023-08-02 16:49:38 -0300

    versão inicial da página Add

--- frete-webapp/src/views/volumes/add.vue
+++ frete-webapp/src/views/volumes/add.vue
@@ -1,36 +1,50 @@
-<script>
-import { computed, inject, onBeforeMount, onMounted, onServerPrefetch, ref } from 'vue';
-import Modal from '@/components/Modal.vue';
-import ModalDelete from '@/components/ModalDelete.vue';
+<script setup>
+import { computed, inject, onBeforeMount, onMounted, onServerPrefetch, reactive, ref } from 'vue';
 import { useUserPermissionsStore } from '@/stores/user';
 import { useItemsAmbienteStore, orderedGroupBy } from '@/stores/items';
 import { useAmbientesStore } from '@/stores/ambientes';
-import { useNumVolumesStore, registra_volume, useVolumesEmailStore, apaga_volume } from '@/stores/volumes';
-import { update_item_part, delete_part, get_parte_ref } from '@/stores/singleitem'
-import Acordeao from '@/components/AcordeaoVolumes.vue';
-import QRCode from '@/components/QRCode.vue';
+import {  registra_volume, apaga_volume } from '@/stores/volumes';
+import { update_item_part, delete_part, get_parte_ref } from '@/stores/singleitem' 
 
 import { db } from '@/backend/index.js';
 import { collection, where, doc, setDoc, query, updateDoc } from 'firebase/firestore';
-import { useCollection } from 'vuefire';
+import {  useDocument, usePendingPromises,  } from 'vuefire';  
 
 const { globaluser, updateUser } = inject("globaluser")
 const permissoes = useUserPermissionsStore()
-const ambientes = useAmbientesStore()
+ 
+const ambientes = reactive({})
+
+const lista_ambientes = [...permissoes.ambientes, ...permissoes.usuario_de];
+
+lista_ambientes.forEach( (amb) => {
+  ambientes[amb] = useDocument(doc(db, "ambientes", amb))
+})
 
 const items = useItemsAmbienteStore()
  
 
 
 const userRef = computed(() => doc(db, "usuarios", globaluser.value.email))
-const volRef = collection(db, "volumes")
-const q = computed(() =>  query(volRef, where("responsavel", "==", userRef.value), where('deleted', '==', false)))
-const volumes = useCollection(q.value, {wait: true})
-
+const volRef = collection(db, "volumes") 
 
 
 const lista_items = ref([])
+ 
 
+
+const ambiente_selected = ref(null)
+
+
+const new_volume = reactive(
+  {
+    categoria: null,
+    responsavel: globaluser.value.email,
+    localizacao_atual: null,
+    origem: null, 
+    items: lista_items.value
+  }
+)
 async function load_all_data() {
   const permissoes = useUserPermissionsStore()
   for (let amb of [...permissoes.ambientes, ...permissoes.usuario_de]) {
@@ -42,15 +56,11 @@ async function load_all_data() {
 
 
 
-const all_items_ordered = computed(() => {
-  if (!permissoes.ambientes) return []
-  let all = []
-  for (let amb of [...permissoes.ambientes, ...permissoes.usuario_de]) {
-    if (items.inner_db[amb]) {
-      // filtrar apenas não volumados
-      all.push(...items.inner_db[amb])
-    }
-  }
+const all_items_ordered = computed(() => { 
+  let all = [] 
+  // filtrar apenas não volumados
+  if(new_volume.origem)
+    all.push(...items.inner_db[new_volume.origem]) 
   return all.sort((a, b) => a.short_descricao.localeCompare(b.short_descricao))
 })
 
@@ -72,27 +82,15 @@ const all_items_filtered = computed(() => {
   }
   return all_items_nao_volumados.value
 })
-
-
-const all_volumes_dict = computed(() => {
-  const dicionario = {} // {'teste': [{short_descricao: 1, key: "a"}]}
-  if (volumes) {
-    volumes.value.forEach((doc) => {
-      // VERIFICAR O QUE TÁ ACONTECENDO AQUI
-      const lista_items = []
-      doc.items.forEach((i) => lista_items.push(i))
-      dicionario[doc.codigo] = lista_items
-    })
-  }
-  return dicionario
-})
+ 
 
 
 
 async function salvar_volume() {
   const volume = {
-    categoria: "NULL",
+    categoria: null,
     responsavel: globaluser.value.email,
+    localizacao: null,
     items: lista_items.value
   }
   console.log(volume)
@@ -119,66 +117,53 @@ async function deleta_volume_parte(parte_key) {
   await delete_part(parte_key)
 }
 
-async function soft_apaga_volume(codigo) {
-  const items_do_volume = all_volumes_dict.value[codigo]
-  console.log(`Apagando volume ${codigo} com ${items_do_volume.length} items`)
-  // vamos desavolumar aqui localmente
-  items_do_volume.forEach(element => {
-    if (element.key.includes("-")) {
-      // SE TEM TRAÇO NA KEY
-      console.log(`Apagando parte ${element.key}`)
-      deleta_volume_parte(element.key)
-    } else { // SE NÃO TEM TRAÇO NO KEY
-      // se for um item normal
-      console.log(`Desavolumento item ${element.key}`)
-      var itemRef = all_items_ordered.value.find(x => x.key == element.key)
-      Object.assign(itemRef, { volumado: false })
-    }
-  });
-  const uptime = apaga_volume(codigo)
-  return true
-}
 
-const soft_volume_modal_ref = ref(null)
-const soft_volume_modal_items = computed(() => {
-  if(soft_volume_modal_ref.value){
-    return all_volumes_dict.value[soft_volume_modal_ref.value]
-  }
-})
-
-
-onBeforeMount(() => volumes.email = permissoes.email)
-onBeforeMount(async () => await load_all_data())
-//onServerPrefetch(() => usePendingPromises())
+ 
+onMounted(async () => await load_all_data())
+onServerPrefetch( () => usePendingPromises() )  
 </script>
 
 <template>
-  <form class="row g-3">
-    <div class="col-6">
-      <label for="codigo" class="form-label">Código</label>
-      <input type="text" class="form-control text-secondary" id="codigo" value="[[...]]" disabled>
-    </div>
+  <div class="row">
+    <div class="col">
+      <h1>Criar Volume <button class="btn btn-danger" @click="()=> console.log(...new_volume)">Criar</button></h1>
+    </div> 
+  </div>
+  <form class="row g-3"> 
 
     <div class="col-6">
-      <label for="responsavel" class="form-label">Responsável</label>
+      <label for="responsavel" class="form-label">Responsável por criar o volume</label>
       <input type="text" class="form-control" id="responsavel" :value="globaluser.email" disabled>
     </div>
-    <div class="col-12">
-      <div class="form-floating">
+    <div class="col-6"> 
+        <label for="floatingSelect" class="form-label">Categoria</label> 
         <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
           <option v-for="x in ['A', 'B', 'C']">{{ x }}</option>
         </select>
-        <label for="floatingSelect">Categoria</label>
-      </div>
+    </div>
+    <div class="col-6">
+      <label for="ambiente" class="form-label">Ambiente</label>  
+      <select v-model="new_volume.origem" class="form-control" id="ambiente">
+        <option  v-for="(val, key, index ) in ambientes" :value="key">
+          {{ val.ambiente_codigo }} - {{ val.ambiente_nome }}
+        </option>
+      </select>  
+    </div>
+    <div class="col-6">
+      <label for="lider-ambiente" class="form-label">Líder do ambiente</label>    
+      <input type="text" class="form-control" id="lider-ambiente" :value="globaluser.email" disabled>
     </div>
     <div class="col-12">
       <h3>Lista de items inclusos no lote</h3>
-    </div>
-    <div class="col-12">
-      <input type="text" class="form-control" placeholder="Digite para filtrar a lista" v-model="filtrar_lista_items">
-    </div>
+      <p class="form-text" v-if="new_volume.origem">
+        Lista de items de {{ ambientes[new_volume.origem].ambiente_nome }}.</p>
+      <p v-else>Selecione o ambiente acima.</p>
+      <input type="text" class="form-control"
+       placeholder="Digite para filtrar a lista abaixo" v-model="filtrar_lista_items">
+
+    </div> 
     <div class="col-12 listatodositems overflow-y-scroll">
-      <table class="table">
+      <table class="table table-hover">
         <thead>
           <tr class="sticky-top">
             <th></th>
@@ -187,9 +172,9 @@ onBeforeMount(async () => await load_all_data())
           </tr>
         </thead>
         <tbody>
-          <tr @click.stop="() => click_row(i)" v-for="(item, i) in all_items_filtered">
+          <tr class="border-bottom border-secondary " @click.stop="() => click_row(i)" v-for="(item, i) in all_items_filtered">
             <td>
-              <input @click.self="() => click_row(i)" class="form-check-input mx-1" type="checkbox" :value="item.key"
+              <input  @click.self="() => click_row(i)" class="form-check-input mx-1 border border-primary" type="checkbox" :value="item.key"
                 v-model="lista_items" :id="'check' + i">
             </td>
             <td>
