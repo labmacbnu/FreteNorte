b08270d51332b0a3293e74eb106b664916aa8999
Bossa <l.f.bossa@ufsc.br>
2023-08-03 16:31:53 -0300

    Adicionei um Input Checkbox
    agora é possível selecionar direto da lista de items

--- frete-webapp/src/components/AcordeaoItems.vue
+++ frete-webapp/src/components/AcordeaoItems.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { ref, watch } from 'vue';
 defineProps(['id', 'lista_agrupada'])
 import AcordeaoChild from './AcordeaoChild.vue';
+
+const emits = defineEmits(['items_selecionados'])
+
+const selecionados = ref([])
+
+watch(selecionados, (novo, antigo) => { 
+    emits('items_selecionados', novo)
+})
 </script>
 
-<template>
+<template> 
 <div class="accordion" :id="id">
         <AcordeaoChild v-for="(valor, chave, n) in lista_agrupada" :pai="id" :aid="'acord' + n">
             <template #titulo>
@@ -12,8 +21,12 @@ import AcordeaoChild from './AcordeaoChild.vue';
             
             <template #corpo>
             <ul class="list-group">
-                <li class="list-group-item d-flex justify-content-between align-items-center" v-for="subitem in valor">
-                    {{ subitem.descricao }}
+                <li class="list-group-item d-flex justify-content-between align-items-center" v-for="(subitem, idx) in valor">
+                    <div class="form-check">
+                        <input :id="'item'+subitem.key" v-model="selecionados" type="checkbox" class="form-check-input border border-primary" :value="subitem.key">
+                        <label :for="'item'+subitem.key" class="form-check-label text-capitalize">{{ subitem.descricao.substring(0,140) }}</label>
+                    </div>
+                    <p></p>
                     <span class="badge badge-primary badge-pill">
                     <RouterLink :to="{name: 'item-codigo', params: {codigo: subitem.key }}">Ver item</RouterLink></span>
                 </li>
