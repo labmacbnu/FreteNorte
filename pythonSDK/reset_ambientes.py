a7cef9f33cd8c6afca81771a48f268001254acac
Bossa <l.f.bossa@ufsc.br>
2023-08-10 10:58:59 -0300

    reseta a lista de ambientes

---
+++ pythonSDK/reset_ambientes.py
@@ -0,0 +1,38 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore, auth
+from datetime import datetime
+import json 
+from dotenv import load_dotenv
+from random import randint
+import os
+
+
+load_dotenv()
+
+cred = credentials.Certificate(os.getenv("firebase_secret"))
+app = firebase_admin.initialize_app(cred)
+
+
+db = firestore.client()
+ 
+COLLECTIONS_MAP = [
+    ('ambientes', 'json/ambientes.json', 'ambiente_codigo'), 
+    ('items', 'json/permanentes.json', 'key')
+]
+
+
+
+def load_json_file(filename):
+    with open(filename) as fp:
+        content = json.load(fp)
+    return content
+
+def unique_ambientes():
+    ambientes = load_json_file('json/ambientes.json')
+    ambientes = [a['ambiente_codigo'] for a in ambientes]
+    return sorted(list(set(ambientes)))
+
+if __name__ == "__main__":
+    unique_ambientes = unique_ambientes() 
+    db.document('agregados/ambientes').update({'codigos': unique_ambientes})
