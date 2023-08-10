c52064e6657ae49e6fde102b368ecca589833249
Bossa <l.f.bossa@ufsc.br>
2023-08-09 21:09:24 -0300

    vários filtros

--- frete-admin/src/views/volumes/home.vue
+++ frete-admin/src/views/volumes/home.vue
@@ -1,20 +1,71 @@
 <script setup>
-
-import { collection, where, doc, setDoc, query, orderBy } from 'firebase/firestore';
-import { useCollection } from 'vuefire';
+import ModalDelete from "@/components/ModalDelete.vue"
+import { collection, where, doc, setDoc, query, orderBy, limit } from 'firebase/firestore';
+import { useCollection, useDocument } from 'vuefire';
 import { db } from '@/backend/index'
 import moment from 'moment';
+import { reactive, computed, ref } from 'vue';
+import { RouterLink } from "vue-router";
+
+const catRef = doc(db, 'agregados/categorias_volumes')
+const {data: categorias, pending: cat_pending, promise: cat_promisse } = useDocument(catRef)
+
+const {data: lista_ambientes, pending: amb_pending} = useDocument(doc(db, 'agregados/ambientes'))
+
+const filtros = reactive({
+  categoria: null,
+  origem: null, 
+  quantidade: 10
+})
+
+
+cat_promisse.value.then(() => filtros.categoria = categorias.value.valores[0])
+
+
 
 const volRef = collection(db, "volumes")
-const q = query(volRef, where('deleted', '==', false), orderBy('data_criacao', 'desc'))
+const q = computed(()=> {
+  var composite_query = [];
+
+  if(filtros.categoria)
+    composite_query.push( where('categoria', '==', filtros.categoria))
+  
+  if(filtros.origem)
+    composite_query.push( where('origem', '==', doc(db, 'ambientes', filtros.origem)))
+
+  return query(volRef,...composite_query,  where('deleted', '==', false), orderBy('data_criacao', 'desc'), limit(filtros.quantidade))
+})
+
 const volumes = useCollection(q, {wait: true})
 
+const soft_volume_modal_ref = ref(null)
 </script>
 <template> 
-
+<div class="row mb-3">
+  <div class="col">
+    <label class="form-label" for="categoria">
+      Filtrar por categoria
+    </label>
+    <select v-model="filtros.categoria" id="categoria" class="form-select" v-if="!cat_pending">
+      <option v-for="categoria in categorias.valores">
+        {{categoria}}
+      </option>
+    </select>
+    <p class="text-end mt-1"><RouterLink class="text-info text-decoration-none" 
+      :to="{name: 'volumes-edita-categorias'}">
+      Editar categorias <i class="bi bi-pencil"></i></RouterLink></p>
+  </div>
+  <div class="col">
+    <label for="ambiente" class="form-label">Filtrar por ambiente de origem</label>
+    <select v-if="!amb_pending" v-model="filtros.origem" id="ambiente" class="form-select">
+      <option v-for="amb in lista_ambientes.liderados">
+      {{ amb }}</option>
+    </select>
+  </div>
+</div>
   <div class="row justify-content-start">
-    <div class="col p-0">
-      <table class="table d-print-table align-middle">
+    <div class="col">
+      <table class="table table-sm d-print-table">
         <thead>
           <tr> 
             <th class="d-print-none">Código</th>
@@ -70,7 +121,7 @@ const volumes = useCollection(q, {wait: true})
                 </li> 
               </ul>
             </td>
-            <td colspan="1"></td>
+            <td colspan="4"></td>
           </tr>
       </template>
       </tbody>
