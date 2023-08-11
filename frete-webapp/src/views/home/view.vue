e7655b52e08282e4af60c3ac42fe23df8ac28382
Bossa <l.f.bossa@ufsc.br>
2023-08-11 11:31:53 -0300

    atualiza automaticamente

--- frete-webapp/src/views/home/view.vue
+++ frete-webapp/src/views/home/view.vue
@@ -1,29 +1,17 @@
 <script setup>
-import { useAmbientesStore, ambiente_status } from '@/stores/ambientes';
+import { useAmbientesUserStore } from '@/stores/ambientes';
 import { useUserPermissionsStore } from '@/stores/user';
 import { RouterLink } from 'vue-router';
-import { computed, ref, reactive, onMounted } from 'vue'; 
-
-const ambientes = useAmbientesStore()
+import { computed, ref, reactive, onMounted, onServerPrefetch, watch } from 'vue'; 
+import { usePendingPromises } from 'vuefire';   
+ 
 const permissions = useUserPermissionsStore()
 
-const meus_ambientes = computed(() => {
 
-    const lista_ambientes = reactive([])
-    if (permissions.ambientes) {
-        lista_ambientes.push(...ambientes.dados.filter(
-            obj => permissions.ambientes.includes(obj.ambiente_codigo) || 
-            permissions.usuario_de.includes(obj.ambiente_codigo)
-        ))
-
-        lista_ambientes.forEach(async (ambiente, index) => {
-            const dicio = await ambiente_status(ambiente.ambiente_codigo)
-            dicio.percent = parseInt(100* dicio.volumados / dicio.todos)
-            lista_ambientes[index]["status"] = reactive(dicio)
-        })
-    }
-    return lista_ambientes
-}) 
+const meus_ambientes = useAmbientesUserStore()
+ 
+meus_ambientes.update_stats()
+ 
 </script>
 
 <template>
@@ -33,15 +21,15 @@ const meus_ambientes = computed(() => {
             <p> Sistema de controle da logística da mudança do campus Blumenau.</p>
             <h2>Seus ambientes</h2>
             <div class="my-3">
-                <div class="card my-2" v-for="(x, i) in meus_ambientes">
+                <div class="card my-2" v-for="(x, i) in meus_ambientes.dados">
                     <div class="card-body" v-if="x">
                         <h5 class="card-title">{{ x.ambiente_codigo }}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">{{ x.ambiente_nome }}</h6>
                         <p class="card-text">
-                        <div v-if="x.status" class="progress" role="progressbar" aria-label="Default striped example"
-                            :aria-valuenow="x.status.volumados" aria-valuemin="0" :aria-valuemax="x.status.todos">
-                            <div class="progress-bar progress-bar-striped" :style="{ width: x.status.percent + '%' }">
-                                {{ x.status.volumados }} de {{ x.status.todos }} volumados</div>
+                        <div v-if="meus_ambientes.status[x.ambiente_codigo]" class="progress" role="progressbar" aria-label="Default striped example"
+                            :aria-valuenow="meus_ambientes.status[x.ambiente_codigo].volumados" aria-valuemin="0" :aria-valuemax="meus_ambientes.status[x.ambiente_codigo].todos">
+                            <div class="progress-bar progress-bar-striped" :style="{ width: meus_ambientes.status[x.ambiente_codigo].percent + '%' }">
+                                {{  meus_ambientes.status[x.ambiente_codigo].volumados }} de {{ meus_ambientes.status[x.ambiente_codigo].todos }} volumados</div> 
                         </div>
                         </p>
                         <RouterLink :to="{ name: 'items-ambiente', params: { ambiente: x.ambiente_codigo } }">Ver ambiente
