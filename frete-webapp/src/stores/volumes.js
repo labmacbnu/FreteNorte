0e4c0001bec44357e228b68426524960a3e6a6ee
Bossa <l.f.bossa@ufsc.br>
2023-08-11 17:35:47 -0300

    Corrige também o Volumes do usuário

--- frete-webapp/src/stores/volumes.js
+++ frete-webapp/src/stores/volumes.js
@@ -1,10 +1,11 @@
 import { computed, ref, watch } from 'vue';
 import { defineStore } from 'pinia';
 import { firebaseApp } from '../firebaseConfig';
-import {  collection, where, doc, setDoc, query, updateDoc, addDoc } from 'firebase/firestore';
+import { collection, where, doc, setDoc, query, updateDoc, addDoc } from 'firebase/firestore';
 import {useCollection, useDocument} from 'vuefire';
 import { db } from '../backend/index.js';
 import {update_item_part} from './singleitem.js';
+import { useUserPermissionsStore } from './user';
  
 
 export const useNumVolumesStore = defineStore("volumes-num",  () => {
@@ -59,14 +60,16 @@ export async function apaga_volume(codigo_volume){
 
 
 export const useVolumesEmailStore = defineStore("volumes-email", () => {
-    const email = ref(null) 
-    const userRef = computed( () => doc(db, "usuarios", email.value) )
-    const volRef = collection(db, "volumes") 
-    const q = computed ( () => {
-        if(email.value)
-            return query(volRef, where("responsavel", "==", userRef.value), where('deleted', '==', false))
-    })
-    const dados = useCollection(q)  
+    const permissoes = useUserPermissionsStore()
+    const email = computed(() => permissoes.email)
+    const dados = ref([])
+
+    watch(email, () => {
+        const userRef = doc(db, "usuarios", email.value) 
+        const volRef = collection(db, "volumes") 
+        const q = query(volRef, where("responsavel", "==", userRef), where('deleted', '==', false))
+        useCollection(q, {wait: true, target: dados})  
+    }, {immediate: true })
     return {email, dados}
 })
 
