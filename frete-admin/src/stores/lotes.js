b98164bdfe40ae1eb9a0b25f9562260f86006ba9
Bossa <l.f.bossa@ufsc.br>
2023-08-14 18:20:24 -0300

    Implementa a opção de criar um lote

---
+++ frete-admin/src/stores/lotes.js
@@ -0,0 +1,32 @@
+import { db } from '@/backend'
+import { ref, computed } from 'vue'
+import { addDoc, runTransaction, collection, doc } from 'firebase/firestore'
+import moment from 'moment'
+
+export const loteModel = {
+    responsavel: String,
+    data_criacao: Date,
+    data_pronto: Date,
+    local_pronto: String,
+    carregamento: String,
+    volumes: Array,
+}
+
+
+export async function save_lote(lote_data) { 
+    const lotesRef = collection(db, "lotes")
+    const volumesRef = collection(db, "volumes")
+    const volumesDocs = lote_data.volumes.map( (volume) => doc(volumesRef, volume) ) 
+    lote_data.data_pronto = moment(lote_data.data_pronto).toDate()
+    lote_data.data_criacao = moment(lote_data.data_criacao).toDate()
+    lote_data.responsavel = doc(db, 'usuarios', lote_data.responsavel)
+    const lote = {...lote_data, volumes: volumesDocs} 
+    const loteAdicionado = await addDoc(lotesRef, lote)
+    const update = await runTransaction(db, async (transaction) => {
+        volumesDocs.forEach( (volume) => {
+            transaction.update(volume, {lote: loteAdicionado, status: 'Loteado'})
+        })
+        return true
+    })
+    return update
+}
--- frete-admin/src/views/lotes/add.vue
+++ frete-admin/src/views/lotes/add.vue
@@ -3,15 +3,23 @@ import { useRoute } from 'vue-router';
 import { useCollection } from 'vuefire';
 import { db } from '@/backend/index';
 import { query, where, collection } from 'firebase/firestore';
-import { computed, inject } from 'vue';
+import { computed, inject, onMounted, reactive, toValue, watch } from 'vue';
+import SelectPlus from '@/components/SelectPlus.vue';
+import { useListaAmbientesStore } from '@/stores/ambientes'
+import { loteModel, save_lote } from '@/stores/lotes'
 import moment from 'moment'
 
 
+const new_lote = reactive(loteModel)
+
 const {globaluser, updateUser} = inject('globaluser')
 const user_label = computed(() => `${globaluser.value.displayName} <${globaluser.value.email}>` )
+
 const route = useRoute()
 
 const volumes_list = route.query.volumes
+ 
+  
 
 const volumes_query = computed( () => {
     const volumesRef = collection(db, 'volumes')
@@ -27,9 +35,37 @@ function formata_data(data){
 
 const hoje = computed(() => moment().format("YYYY-MM-DD"))
 
+
+const all_ambientes = useListaAmbientesStore()
+
+const todos_os_ambientes = computed(() => {
+    return  all_ambientes.dados.map(amb => ({name: amb, id: amb})) 
+ })
+
+const carretamentos = [
+    {name: 'Carregamento 1', id: 1, data: '2023-09-01'},
+    {name: 'Carregamento 2', id: 2, data: '2023-09-10'},
+    {name: 'Carregamento 3', id: 3, data: '2023-09-21'},
+    {name: 'Carregamento 4', id: 4, data: '2023-09-30'},
+] 
+
+async function front_save_lote(){
+    //console.log(toValue(new_lote)) 
+    const valor = await save_lote({...new_lote})
+    console.log(valor)
+}
+
+
+onMounted(() => {
+    new_lote.volumes = route.query.volumes
+    new_lote.responsavel = globaluser.value.email
+    new_lote.data_criacao = hoje.value
+})
 </script>
 <template>
-    <h2>Criando um lote</h2> 
+    <div class="d-flex justify-content-between">
+    <h4 class="display-4">Criando um lote </h4> <button @click="front_save_lote" class=" fs-4 px-4 btn btn-success">Salvar</button>
+    </div>
     <div class="row">
         <div class="col-6 mb-2">
             <label for="data_criacao" class="form-label">Data criação</label>
@@ -39,13 +75,24 @@ const hoje = computed(() => moment().format("YYYY-MM-DD"))
             <label for="responsavel" class="form-label">Responsável</label>
             <input disabled id="responsavel" :value="user_label"  class="form-control">
         </div>
-        <div class="col-6  mb-2">
-            <label for="data_pronto" class="form-label">Data pronto</label>
-            <input id="data_pronto" type="date"  class="form-control">
+    </div>
+    <div class="row">
+        <div class="col">
+            <h4>Carregamento</h4>
+        </div>  
+    </div>
+    <div class="row">
+        <div class="col-4  mb-2">
+            <label for="carregamento" class="form-label">Carregamento</label>
+            <SelectPlus placeholder="Selecione uma opção" :options="carretamentos" @selected="(c) => {new_lote.carregamento = c.name; new_lote.data_pronto = c.data }"></SelectPlus>
         </div>
-        <div class="col-6  mb-2">
-            <label for="localizacao" class="form-label">Localização</label>
-            <input id="localizacao" type="text"  class="form-control">
+        <div class="col-4  mb-2">
+            <label for="data_pronto" class="form-label">Data despacho</label>
+            <input v-model="new_lote.data_pronto" id="data_pronto" type="date"  class="form-control">
+        </div>
+        <div class="col-4  mb-2">
+            <label for="localizacao" class="form-label">Local despacho</label>
+            <SelectPlus placeholder="Selecione um ambiente" :options="todos_os_ambientes" @selected="(c) => new_lote.local_pronto = c.name"></SelectPlus>
         </div>
     </div>
     <div class=" my-2">
