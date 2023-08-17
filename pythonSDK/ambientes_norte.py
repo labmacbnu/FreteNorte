95f627f53abefc67b4f187547809a04e847d7879
Bossa <l.f.bossa@ufsc.br>
2023-08-17 17:39:37 -0300

    ipynb para py script

---
+++ pythonSDK/ambientes_norte.py
@@ -0,0 +1,64 @@
+# %%
+import pandas as pd
+from pathlib import Path
+
+# %%
+SOURCE = Path("xlsx/norte/ambientes_salto.xlsx")
+
+# %%
+df = pd.read_excel(SOURCE, skiprows=1, usecols="B:Z")
+
+# %%
+df = df[~df.Código.isna()]
+
+# %%
+df.drop(columns=df.columns[15:], inplace=True)
+
+# %%
+def rewrite_columns(string):
+    replacing = [(" ", "_"), ("ã", "a"), ("á", "a"), ("õ", "o"), ("²", ""), ("ç", "c"), ("é", "e"), ("í", "i"), ("ó", "o"), ("ú", "u"), ("(", ""), (")", ""), ("%", "")]
+    for pair in replacing:
+        string = string.replace(pair[0], pair[1])
+    return string.lower()
+
+
+df.columns = [rewrite_columns(x) for x in df.columns]
+
+# %%
+ambientes_origem = [x for x in df.columns if 'ambiente_origem' in x]
+
+df["origem"] =  df[ambientes_origem].apply(lambda x: [ amb.split(' - ')[0] for amb in x.values if type(amb) == str ], axis=1)
+
+df.drop(columns=ambientes_origem, inplace=True)
+
+# %%
+df
+
+# %%
+dicionarios = df.to_dict('records')
+
+# %%
+registros = []
+
+for item in dicionarios:
+    key = item.pop('codigo')
+    nome = item.pop('nome')
+    origem = item.pop('origem')
+    registros.append({
+        'nome': nome,
+        'origem': origem,
+        'codigo': key,
+        'detalhes':  item})
+
+# %%
+import json 
+
+OUTPUT_PATH = Path("json") / "ambientes-norte.json"
+
+with open(OUTPUT_PATH, "w") as fP:
+    json.dump(registros, fP, indent=2, ensure_ascii=False)
+
+# %%
+
+
+
