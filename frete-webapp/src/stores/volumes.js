73b7c8b58ace160c95eeb04e88f4895cd81b6386
Bossa <l.f.bossa@ufsc.br>
2023-08-22 16:17:51 -0300

    referências para as imagens

--- frete-webapp/src/stores/volumes.js
+++ frete-webapp/src/stores/volumes.js
@@ -89,4 +89,12 @@ export const simbolos_nbr = {
     'Proteger do Calor': '/proteger-contra-o-calor.webp',
     'Proteger da Luz': '/proteger-da-luz.webp',
     'Proteger da Umidade': '/protegerdaumidade.webp',
+    'Corrosivo': '/corrosivo.webp',
+    'Cargas Pesadas': '/cargas-pesadas.webp',
+    'Explosivo': '/explosivo.webp',
+    'Inflamável': '/inflamavel.webp',
+    'Oxidante': '/oxidante.webp',
+    'Risco Ambiental': '/risco-ambiental.webp',
+    'Radioativo': '/radioativo.webp',
+    'Gás Comprimido': '/gas-comprimido.webp',
   }
