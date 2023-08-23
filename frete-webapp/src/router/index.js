a40f5d0d5ff19084dcdce4937a687cb53ccd6fa3
Bossa <l.f.bossa@ufsc.br>
2023-08-23 15:26:16 -0300

    cria a rota /ajuda

--- frete-webapp/src/router/index.js
+++ frete-webapp/src/router/index.js
@@ -119,7 +119,20 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ScanearView.vue'),
       meta: {
-        requiresAuth: true
+        requiresAuth: true,
+        title: "Ler QR Code"
+      }
+    },
+    {
+      path: '/ajuda',
+      name: 'ajuda',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('@/views/AjudaView.vue'),
+      meta: {
+        requiresAuth: true,
+        title: "Ajuda"
       }
     }
   ]
