447202ff42d6043ad8cd56b8b41fe59de73d7615
Bossa <l.f.bossa@ufsc.br>
2023-08-23 17:33:20 -0300

    mockup

---
+++ frete-admin/src/stores/volumes.js
@@ -0,0 +1,15 @@
+export const simbolos_nbr = {
+    'Frágil': '/fragil.webp',
+    'Proibido Empilhar': '/proibido-empilhar.webp',
+    'Proteger do Calor': '/proteger-contra-o-calor.webp',
+    'Proteger da Luz': '/proteger-da-luz.webp',
+    'Proteger da Umidade': '/protegerdaumidade.webp',
+    'Corrosivo': '/corrosivo.webp',
+    'Cargas Pesadas': '/cargas-pesadas.webp',
+    'Explosivo': '/explosivo.webp',
+    'Inflamável': '/inflamavel.webp',
+    'Oxidante': '/oxidante.webp',
+    'Risco Ambiental': '/risco-ambiental.webp',
+    'Radioativo': '/radioativo.webp',
+    'Gás Comprimido': '/gas-comprimido.webp',
+}
