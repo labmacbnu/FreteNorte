b17efe0720c44e65c095c93912b121f02eb634e0
Bossa <l.f.bossa@ufsc.br>
2023-08-25 10:33:25 -0300

    clona o codigo.vue

--- frete-webapp/src/views/volumes/codigo-edit.vue
+++ frete-webapp/src/views/volumes/codigo-edit.vue
@@ -1,15 +1,21 @@
 <script setup>
-import { useRoute, useRouter } from 'vue-router'
 import { doc } from 'firebase/firestore'
 import { useDocument } from 'vuefire'
-import { db, getdoc } from '@/backend/index'
-import { onBeforeMount, onMounted, ref } from 'vue'
+import { db } from '@/backend/index'
+import { computed, ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import QRCode from '@/components/QRCode.vue'
 import moment from 'moment'
+import { simbolos_nbr } from '@/stores/volumes'
 
 const route = useRoute()
 const router = useRouter()
-const codigo = route.params.codigo
+const codigo = ref( (route.params)? route.params.codigo: null)
+
+watch( () => route.params.codigo, () =>{
+  codigo.value = route.params.codigo
+}, {immediate: true})
+
 const {
   // rename the Ref to something more meaningful
   data: volume,
@@ -19,13 +25,16 @@ const {
   error,
   // A promise that resolves or rejects when the initial state is loaded
   promise
-} = useDocument(doc(db, "volumes", codigo))
+} = useDocument(doc(db, "volumes", codigo.value))
 
 const responsavel = ref(null)
 
 function print(){
   window.print()
 }
+
+
+
 
 </script>
 <template>
@@ -50,25 +59,47 @@ function print(){
     </template>
     <template v-else>
       <!-- Existe -->
-
-      <div class="row justify-content-start g-3">
-        <div class="col-8">
-          <h3>Volume {{ volume.codigo }}</h3>
-          <p class="mb-1"><b>Responsável:</b> {{ volume.responsavel.nome }}</p>
-          <p class="mb-1"><b>Origem:</b> {{ volume.origem.ambiente_codigo }} &dash; {{ volume.origem.ambiente_nome }}</p>
-          <p class="mb-1"><b>Criado em:</b> {{ moment.unix(volume.data_criacao.seconds).format("DD/MM/YY HH:MM") }}</p>
-          <p class="mb-1"><b>Local atual:</b> {{ volume.localizacao_atual.ambiente_codigo }}</p>
-          <p class="mb-1"><b>Destino:</b> </p>
-        </div>
-
-        <div class="col d-flex justify-content-end">
-          <QRCode :path="route.fullPath"></QRCode>
-        </div>
-
-      </div>
       <div class="row">
         <div class="col">
-          <p class="fw-bold">Lista de itens</p>
+          <h4>Volume {{ volume.codigo }}</h4>
+        </div>
+      </div>
+      <div class="row justify-content-start g-1">
+        <div class="col-4">
+          <p class="mb-1"><b>Responsável:</b> {{ volume.responsavel.nome }}</p>
+          <p class="mb-1"><b>Criado em:</b> {{ moment.unix(volume.data_criacao.seconds).format("DD/MM/YY HH:MM") }}</p>
+          <h5 class="mt-3">Localização</h5>
+          <div class="hstack gap-2">
+            <p class="px-3 text-center mb-1"><b>Origem:</b> {{ volume.origem.ambiente_codigo }}</p>
+              <i class="bi bi-arrow-right"></i>
+            <p class="px-3 align-self-top text-center mb-1 d-print-none"><b>Atual:</b> {{ volume.localizacao_atual.ambiente_codigo }}</p>
+              <i class="bi bi-arrow-right d-print-none"></i>
+            <p class="px-3 text-center mb-1"><b>Destino:</b>  {{ volume.destino ? volume.destino.codigo : "" }} </p>
+          </div>
+
+        </div>
+        <div class="col text-end">
+          <template v-for="(valor, key) in simbolos_nbr">
+           <template v-if="volume.propriedades?.includes(key)">
+            <figure class="figure">
+              <img  :src="valor" :title="key" :alt="key" class="figure-img img-fluid" 
+              style="max-width: 96px; max-height: 96px; object-fit: contain; object-position: center;"/> 
+              <figcaption class="figure-caption text-center">{{ key }}</figcaption>
+            </figure> 
+           </template>
+          </template>
+        </div>
+        <div class="col-3 d-flex justify-content-end"> 
+          <QRCode :path="route.fullPath"></QRCode>
+        </div>
+        <div class="col-12" v-if="volume.observacao">
+          <p class="mt-3"><b class="fs-5">Observação:</b> {{ volume.observacao  }}</p>
+        </div>
+
+      </div> 
+      <div class="row mt-2">
+        <div class="col">
+          <h5 class="fw-bold">Lista de itens</h5>
           <ul class="list-group">
             <li class="list-group-item justify-content-between d-flex" v-for="item in volume.items">
               <span>
