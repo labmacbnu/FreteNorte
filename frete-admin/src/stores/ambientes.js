2078400c81666765000bfa703b6d81969220c5a2
Bossa <l.f.bossa@ufsc.br>
2023-08-29 18:05:56 -0300

    adiciona ambiente status

--- frete-admin/src/stores/ambientes.js
+++ frete-admin/src/stores/ambientes.js
@@ -1,9 +1,9 @@
-import { computed, reactive, ref } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
 import { defineStore } from 'pinia'
-import { getFirestore, query, where, collection, setDoc, doc, updateDoc } from 'firebase/firestore'
+import { getFirestore, query, where, collection, setDoc, doc, updateDoc, getCountFromServer } from 'firebase/firestore'
 import { firebaseApp } from '../firebaseConfig'
 import { useCollection, useDocument } from 'vuefire'
-import { useUsuariosStore } from './users'
+import { useUsuariosStore } from './users'  
 
 
 
@@ -11,22 +11,22 @@ export const useAmbientesStore = defineStore('ambientes', ()=>{
     const db = getFirestore(firebaseApp)
     const edificio = ref('')
     const pesquisa = computed(() => query(collection(db, "ambientes"), where("edificio", "==", edificio.value)))
-    const pre_dados = useCollection(pesquisa)
-    const dados = computed(() => {
-        const ambientes = []
-        pre_dados.value.forEach( function (doc) { 
-            var docdt = doc
-            docdt.valor = docdt.ambiente_codigo + ' - ' + docdt.ambiente_nome  
-            ambientes.push(docdt)
-        }) 
-        return ambientes
-    })
-    return {dados, edificio} 
+    const dados = useCollection(pesquisa)
+    const status = reactive({})
+    watch(dados, () =>  {
+        if(dados.value){
+            dados.value.forEach( ambiente => {
+                ambiente_status(ambiente.codigo).then( (value) => { status[ambiente.codigo] = value } )
+            })
+        }
+    }) 
+    return {dados, edificio, status} 
 })
 
 export async function create_ambiente(ambiente_data) { 
     const db = getFirestore(firebaseApp)
-    const docRef = doc(db, 'ambientes', ambiente_data.ambiente_codigo)
+    const docRef = doc(db, 'ambientes', ambiente_data.codigo)
+    ambiente_data.lider = doc(db, 'usuarios', ambiente_data.lider)
     const uptime = await setDoc(docRef, {...ambiente_data})
     return uptime
 
@@ -48,3 +48,22 @@ export const useListaAmbientesStore = defineStore('listaAmbientes', ()=>{
     const dados = computed(() =>  pending.value? [] : pre_dados.value.codigos)
     return {dados}
 })
+
+async function ambiente_status(ambiente_codigo) {
+    const db = getFirestore(firebaseApp)
+    const ambienteRef = doc(db, "ambientes", ambiente_codigo)
+    const volumados_query = query(collection(db, "items"), where('ambiente', '==', ambienteRef),
+    where('meta.volumado', '==', true))
+    const volumados_snap = await getCountFromServer(volumados_query)
+    const volumados_n = volumados_snap.data().count
+
+    const all_query = query(collection(db, "items"), where('ambiente', '==', ambienteRef),
+    where('meta.inteiro', '==', true)) // where('inteiro', '==', true)
+    const all_snap = await getCountFromServer(all_query)
+    const all_n = all_snap.data().count
+
+    const percent = parseInt(100* volumados_n / all_n)
+
+    return {volumados: volumados_n, todos: all_n, percent: percent, ambiente: ambiente_codigo}
+
+}
