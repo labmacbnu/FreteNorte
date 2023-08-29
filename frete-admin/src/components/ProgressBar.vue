b7ff0c08f2e419218461bffd845ab7b84bb52cfd
Bossa <l.f.bossa@ufsc.br>
2023-08-29 18:05:44 -0300

    cria a barra de progresso

---
+++ frete-admin/src/components/ProgressBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="progress" role="progressbar" aria-label="Default striped example"
+        :aria-valuenow="atual" aria-valuemin="0"
+        :aria-valuemax="maximo">
+        <div class="progress-bar progress-bar-striped" :style="{ width: percentual + '%' }">
+            {{ atual }} de {{ maximo }} volumados</div>
+    </div>
+</template>
+<script setup>
+defineProps(['atual', 'maximo', 'percentual'])
+</script>
