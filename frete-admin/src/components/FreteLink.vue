a1e0e3612278bf841e62b08b09f7d61c3e7168a3
Bossa <l.f.bossa@ufsc.br>
2023-08-30 17:06:47 -0300

    link para um path em frete-norte webapp

---
+++ frete-admin/src/components/FreteLink.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    path: String,
+    decoration: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+<template>
+<a target="_blank" 
+:class="decoration ? 'text-decoration-underline' : 'text-decoration-none'" 
+:href="'https://frete-norte-ufsc-blumenau.web.app/' + path">
+<slot></slot>
+</a>
+</template>
