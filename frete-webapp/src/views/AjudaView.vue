06b286d35451483a30a7a701da6523f587105835
Bossa <l.f.bossa@ufsc.br>
2023-09-06 18:09:31 -0300

    muda o link da ajuda

--- frete-webapp/src/views/AjudaView.vue
+++ frete-webapp/src/views/AjudaView.vue
@@ -44,7 +44,7 @@ function enviar_duvida(){
 </script>
 <template>
     <h1>Ajuda</h1>
-    <p>Você pode utilizar o campo abaixo para filtrar as perguntas. Se não encontrar, <a href="#" data-bs-toggle="modal" data-bs-target="#enviarduvida">clique aqui</a> para enviar uma nova dúvida. </p>
+    <p>Você pode utilizar o campo abaixo para filtrar as perguntas. Se não encontrar, <a href="#" data-bs-toggle="modal" data-bs-target="#enviarduvida">clique aqui para enviar uma nova dúvida</a>. </p>
     <input type="text" class="form-control mb-3" v-model="search" placeholder="Pesquisar nas perguntas" aria-label="Pesquisar" aria-describedby="button-addon2">
     <div class="accordion border-bottom-0">
         <AjudaItem v-for="documento in ajuda_filtrada" v-bind="documento" :key="documento.id"></AjudaItem>
