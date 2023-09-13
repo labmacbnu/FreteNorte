97f808cc61086d4769343f9b8fbbaf8854431bbc
Bossa <l.f.bossa@ufsc.br>
2023-09-13 12:44:33 -0300

    inclui outros canais de ajuda

--- frete-webapp/src/views/AjudaView.vue
+++ frete-webapp/src/views/AjudaView.vue
@@ -44,6 +44,18 @@ function enviar_duvida(){
 </script>
 <template>
     <h1>Ajuda</h1>
+    <h3>Canais de ajuda</h3>
+    <dl class="ms-3">
+        <dt>Área dos líderes de ambiente</dt>
+        <dd>Área dedicada dentro do site da mudança: <a href="https://mudancanorteblumenau.paginas.ufsc.br/logistica/area-dos-lideres-de-ambiente/">Área dos líderes de ambiente</a>.</dd>
+        <dt>e-mail</dt>
+        <dd>Coordenação logística: <a href="mailto:coord.log.bnu@contato.ufsc.br">coord.log.bnu@contato.ufsc.br</a> </dd>
+        <dd>COGEP:  <a href="mailto:cogep.bnu@contato.ufsc.br">cogep.bnu@contato.ufsc.br</a></dd>
+        <dt>WhatsApp</dt>
+        <dd><a href="https://chat.whatsapp.com/LooZsGz4coA8hB5ToiAnFk​">Link para o grupo dos Líderes</a></dd>
+    </dl>
+
+    <h3>Dúvidas frequentes</h3>
     <p>Você pode utilizar o campo abaixo para filtrar as perguntas. Se não encontrar, <a href="#" data-bs-toggle="modal" data-bs-target="#enviarduvida">clique aqui para enviar uma nova dúvida</a>. </p>
     <input type="text" class="form-control mb-3" v-model="search" placeholder="Pesquisar nas perguntas" aria-label="Pesquisar" aria-describedby="button-addon2">
     <div class="accordion border-bottom-0">
