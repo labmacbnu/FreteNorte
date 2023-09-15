8f36367eed0df617fbea0a2b61a89c2d89dc93b2
Bossa <l.f.bossa@ufsc.br>
2023-09-15 10:03:25 -0300

    melhora a barra de progresso
    inclui cores e texto personalizado

--- frete-admin/src/components/ProgressBar.vue
+++ frete-admin/src/components/ProgressBar.vue
@@ -2,10 +2,31 @@
     <div class="progress" role="progressbar" aria-label="Default striped example"
         :aria-valuenow="atual" aria-valuemin="0"
         :aria-valuemax="maximo">
-        <div class="progress-bar progress-bar-striped" :style="{ width: percentual + '%' }">
-            {{ atual }} de {{ maximo }} volumados</div>
+        <div :class="cor" class="progress-bar progress-bar-striped" :style="{ width: percentual + '%' }">
+            {{ texto }}</div>
     </div>
 </template>
 <script setup>
-defineProps(['atual', 'maximo', 'percentual'])
+import { computed } from 'vue';
+const props = defineProps(['atual', 'maximo', 'percentual'])
+
+const percentual = computed( () => (props.atual == props.maximo)? 100: props.percentual )
+const texto = computed( () => props.maximo == 0 ? `Ambiente vazio`: `${props.atual} de ${props.maximo} volumados`)
+
+const cor = computed( () => {
+    if(props.maximo == 0) {
+        return `bg-secondary`
+    }
+    if (props.percentual >= 99){
+        return `bg-success`
+    }
+    if (props.percentual >= 60){
+        return `bg-primary`
+    }
+    if (props.percentual >= 40){
+        return `bg-warning`
+    }
+    return `bg-danger`
+
+})
 </script>
