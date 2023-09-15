2e2d2721602f8e31bcc0bf00214c879fcd05ea54
Bossa <l.f.bossa@ufsc.br>
2023-09-15 09:36:52 -0300

    lideres de ambiente

---
+++ pythonSDK/load_lideres.py
@@ -0,0 +1,70 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore, auth
+from datetime import datetime
+import json 
+from dotenv import load_dotenv
+from random import randint
+import os
+import re
+from unidecode import unidecode
+
+
+
+load_dotenv()
+
+cred = credentials.Certificate(os.getenv("firebase_secret"))
+app = firebase_admin.initialize_app(cred)
+
+
+def load_json_file(filename):
+    with open(filename) as fp:
+        content = json.load(fp)
+    return content
+
+db = firestore.client()
+
+
+LIDERES = load_json_file("json/lideres.json")
+
+
+def load_usuarios():
+    userbase = {}
+    usuarios = db.collection("usuarios")
+    for doc in usuarios.stream():
+        dados = doc.to_dict()
+        userbase[doc.id] = dados
+    return userbase
+
+USERBASE = load_usuarios()
+
+def filtro_nome(pesquisa):
+    def filtro(pair):
+        email, dados = pair 
+        if re.match(pesquisa, dados["nome"], re.I):
+            return True
+        else:
+            return False 
+    return filtro
+
+def find_usuario(nome):
+    regex_nome = nome.replace(".", ".*") 
+    filtro = filtro_nome(regex_nome)
+    return dict( filter( filtro, USERBASE.items() ) )
+
+def confirmar(nome_lider, user_nome):
+    resposta = input(f"Confirma que {nome_lider} é {user_nome}?")
+    return resposta != "N"
+
+if __name__ == "__main__":
+    N = len(LIDERES)
+    M = 0
+    for nome_lider in LIDERES.keys():
+        resposta = find_usuario(unidecode(nome_lider)) 
+        if resposta != {}:
+            for email, user_data in resposta.items():
+                M += 1
+                confirmar(nome_lider, user_data['nome'])
+
+    print(f"Total de líderes de ambientes: {N}")
+    print(f"Total de líderes logados: {M}")
