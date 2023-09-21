75352f222e65e884be4f83e34c0a6dc64dafa586
Bossa <l.f.bossa@ufsc.br>
2023-09-21 16:51:00 -0300

    funções para pegar um documento

--- frete-webapp/src/backend/index.js
+++ frete-webapp/src/backend/index.js
@@ -1,11 +1,12 @@
 import {firebaseApp} from '../firebaseConfig';
 import {getFirestore, collection, getDoc, doc } from 'firebase/firestore';
+import { useCollection, useDocument } from 'vuefire';
 
 export const db = getFirestore(firebaseApp);
 
 export const volumesRef = collection(db, "volumes") 
 
-export async function getdoc(collRef, docRef) {
+export async function getdoc(/* @type: String*/ collRef, docRef) {
     console.log(`${collRef}/${docRef}`)
     const docSnap = await getDoc(doc(db, collRef, docRef))
     if (docSnap.exists()) { 
@@ -13,4 +14,17 @@ export async function getdoc(collRef, docRef) {
     } else {
         return null
     }
+}
+
+
+export async function get_document(docRef) { 
+    const {data: doc, promise} = useDocument(docRef)
+    await promise.value
+    return doc.value
+}
+ 
+export async function get_query(query) {
+    const {data: docs, promise} = useCollection(query, {once: true})
+    await promise.value
+    return docs.value
 }
