c20d714f6d1b74abdd190e830525bc9b0cc482e3
Bossa <l.f.bossa@ufsc.br>
2023-11-22 18:09:37 -0300

    Refactor volume table layout
    mostra itens e medidas

--- frete-webapp/src/views/volumes/home.vue
+++ frete-webapp/src/views/volumes/home.vue
@@ -79,12 +79,13 @@ const soft_volume_modal_ref = ref(null)
           <tr>
             <th class="d-none d-print-table-cell">Volume</th>
             <th class="d-print-none">Código</th>
-            <th class="d-print-none">Editar</th>
-            <th>Itens</th>
+            <th class="d-print-none">Editar</th> 
             <th>Origem</th>
             <th>Local<br>atual</th>
             <th>Destino</th>
             <th>Categoria</th>
+            <th>Itens</th>
+            <th>Medidas</th>
             <th>Status</th>
             <th>Criado em</th>
             <th class="d-print-none">Apagar</th>
@@ -99,7 +100,7 @@ const soft_volume_modal_ref = ref(null)
             </td>
             <td class="d-print-none"> 
               <RouterLink class="" :to="{name: 'volume-codigo', params: {codigo: volume.codigo }}">
-              {{ volume.codigo.substring(0,10) + '...' }}
+              {{ volume.codigo }}
             </RouterLink>
             </td>
             <td> 
@@ -107,15 +108,28 @@ const soft_volume_modal_ref = ref(null)
                 Editar <i class="bi bi-pencil-square"></i>
             </RouterLink>
             </td>
-            <td> 
+            <!-- <td> 
               <a :href="'#items' + volume.codigo"  class="btn btn-sm btn-primary" data-bs-toggle="collapse" 
                 role="button" aria-expanded="false" :aria-controls="'items' + volume.codigo">
                 Detalhes</a>
-            </td>
+            </td> -->
             <td class="text-center"><AmbienteFlag v-bind="volume.origem"></AmbienteFlag></td>
             <td><AmbienteFlag v-bind="volume.localizacao_atual"></AmbienteFlag></td>
             <td><AmbienteFlag v-bind="volume.destino"></AmbienteFlag></td>
             <td>{{ volume.categoria }}</td>
+            <td>
+              <ul class="list-group list-group-flush align-top">
+              <template  v-for="item in volume.items"> 
+                <li v-if="item" class="list-group-item justify-content-between d-flex">
+                    <small class="text-lowercase">{{ item.short_descricao }}</small>
+                    <RouterLink :to="{name: 'item-codigo', params: {codigo: item.key}}" 
+                    class="badge text-primary rounded-pill span-lista-volumes text-elipse">{{item.key}}</RouterLink>
+                  </li> 
+              </template>
+              </ul>
+            </td>
+            <td><VMedidasDisplay v-bind="volume.medidas"></VMedidasDisplay>
+            </td>
             <td><StatusList :status="volume.status"></StatusList></td>
             <td :title="moment.unix(volume.data_criacao.seconds).format('DD/MM/YY HH:MM')">{{ moment.unix(volume.data_criacao.seconds).format("DD/MM/YY") }}</td>  
             <td class="d-print-none">
@@ -123,7 +137,7 @@ const soft_volume_modal_ref = ref(null)
               @click="() => soft_volume_modal_ref = volume.codigo"><i class="bi bi-trash" title="Apagar volume"></i></button>
             </td>
           </tr>
-          <tr class="collapse" :id="'items' + volume.codigo">
+          <!-- <tr class="collapse" :id="'items' + volume.codigo">
             <td colspan="1"></td>
             <td colspan="5"> 
             <ul class="list-group list-group-flush align-top border-top">
@@ -144,7 +158,7 @@ const soft_volume_modal_ref = ref(null)
              </ul>
             </p>
             </td>
-          </tr>
+          </tr> -->
       </template>
       </tbody>
       </table>
