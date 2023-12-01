a476a4955d4b1e9af74a0f4cf58aefc5ed57ebcc
Bossa <l.f.bossa@ufsc.br>
2023-12-01 14:19:15 -0300

    deixa vermelho se a medida for muito grande

--- frete-webapp/src/components/VMedidasDisplay.vue
+++ frete-webapp/src/components/VMedidasDisplay.vue
@@ -1,6 +1,12 @@
 <template>
-    <span class="text-nowrap">{{ a }}&times;{{ l }}&times;{{ c }} cm</span>
+    <span class="text-nowrap" :class="[okay ? 'text-dark' : 'text-danger']">{{ p.a }}&times;{{ p.l }}&times;{{ p.c }} cm</span>
 </template>
 <script setup>
-defineProps(['a','l','c'])
+import { computed } from 'vue';
+
+const p = defineProps(['a','l','c'])
+
+const okay = computed(() => {
+    return p.a >= 10 && p.l >= 10 && p.c >= 10
+})
 </script>
