18fc8c6a9a8c0a689116b02a9f52d133d34f0709
Bossa <l.f.bossa@ufsc.br>
2024-02-29 11:38:30 -0300

    Adicionado tratamento especial para volumes

--- frete-webapp/src/views/volumes/codigo.vue
+++ frete-webapp/src/views/volumes/codigo.vue
@@ -47,6 +47,8 @@ function checkprivileges() {
 
 }
 
+const especial = useDocument(doc(db, "especiais", codigo.value))
+
 
 </script>
 <template>
@@ -75,14 +77,20 @@ function checkprivileges() {
       <div class="row">
         <div class="col d-flex justify-content-between">
           <h4>Volume {{ volume.codigo }}</h4>
+          <div class="d-print-none" v-if="especial"> 
+              <a target="_blank" class="fs-5 icon-link m-1" :href="especial.link">Tratamento especial<i
+                  class="bi bi-file-pdf"></i></a> 
+          </div>
           <RouterLink v-if="checkprivileges" class="btn btn-secondary d-print-none"
             :to="{ name: 'volume-edit', params: { codigo: volume.codigo } }"><i class="bi bi-pencil"></i> Editar Volume
           </RouterLink>
         </div>
       </div>
+
       <ConteudoVolume :volume="volume"></ConteudoVolume>
 
     </template>
+
     <p @click="print" role="button" class="d-print-none mt-4 form-text text-end">Essa página foi otimizada para impressão.
       Clique aqui para imprimir <i class="bi bi-printer"></i>.</p>
   </template>
@@ -97,4 +105,5 @@ function checkprivileges() {
 .codigo-span {
   min-width: 4.5em;
   max-width: 6em;
-}</style>
+}
+</style>
