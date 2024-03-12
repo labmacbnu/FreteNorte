b0988caedc4db8d08d8cdc83f2ba13a7e9237d12
Bossa <l.f.bossa@ufsc.br>
2024-03-12 15:00:43 -0300

    Adiciona componente LotesCarregamento.vue com importações e uso do store de lotes

---
+++ frete-webapp/src/views/lotes/LotesCarregamento.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router' 
+import { useLotesStore } from '@/stores/lotes';
+const lotes = useLotesStore()
+
+</script>
+<template>
+
+    <p>{{ lotes.volumesCods }}</p>
+</template>
