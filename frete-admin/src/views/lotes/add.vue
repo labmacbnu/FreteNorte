0130946e5a7543b02a49ef47b82cb704e2646ad4
Bossa <l.f.bossa@ufsc.br>
2024-03-13 17:01:20 -0300

    Nova versão para carregamentos

---
+++ frete-admin/src/views/lotes/add.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <h1>Lotes Add</h1>
+
+        </div>
+        <div class="col">
+            <RouterLink :to="{name: 'lotes-add'}" class="btn btn-primary">Adicionar lote</RouterLink>
+        </div>
+    </div>
+</template>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
