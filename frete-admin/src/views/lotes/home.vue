291e89dc1b632c5c0ffd568977b12684b632b53d
Bossa <l.f.bossa@ufsc.br>
2024-03-15 11:28:33 -0300

    Atualiza a página inicial de lotes para exibir carregamentos

--- frete-admin/src/views/lotes/home.vue
+++ frete-admin/src/views/lotes/home.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="row">
-        <div class="col">
-            <h1>Lotes Home</h1>
+        <div class="col-8">
+            <h1>Carregamentos</h1>
 
         </div>
-        <div class="col">
-            <RouterLink :to="{name: 'lotes-add'}" class="btn btn-primary">Adicionar lote</RouterLink>
+        <div class="col-4 text-end">
+            <RouterLink :to="{name: 'lotes-add'}" class="btn btn-primary"><i class="bi bi-plus"></i>Adicionar carregamento</RouterLink>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col"> 
+            <CarregamentoShow v-for="carregamento in carregamentos" :key="carregamento.id" :carregamento="carregamento" />
         </div>
     </div>
 </template>
 <script setup>
 import { RouterLink } from 'vue-router';
+import { useCollection } from 'vuefire';
+import { db } from '@/backend';
+import { collection } from 'firebase/firestore';
+import CarregamentoShow  from '@/components/CarregamentoShow.vue';
+
+const carregamentos = useCollection(collection(db, 'carregamentos'));
 </script>
