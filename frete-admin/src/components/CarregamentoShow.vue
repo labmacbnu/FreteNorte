7858a691d46de3ec483d90e3558dd326eee5428e
Bossa <l.f.bossa@ufsc.br>
2024-03-15 11:28:17 -0300

    Adicionado componente CarregamentoShow.vue para exibir informações de carregamento

---
+++ frete-admin/src/components/CarregamentoShow.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="card w-50" >
+  <div class="card-body">
+    <h5 class="card-title">{{carregamento.id}}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">{{formata_data(carregamento.data_saida)}}</h6>
+    <p class="card-text">
+       
+    </p> 
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item  d-flex justify-content-between align-items-center">
+        Caminhão<span class="badge bg-secondary">{{carregamento.caminhao.placa}}</span>
+    </li>
+    <li class="list-group-item  d-flex justify-content-between align-items-center">
+        Status<span class="badge bg-secondary">{{carregamento.status}}</span>
+    </li> 
+    <li class="list-group-item  d-flex justify-content-between align-items-center">
+        Saída<span class="badge bg-secondary">{{carregamento.saida}}</span>
+    </li> 
+  </ul>
+</div>
+</template>
+<script setup>
+import moment from 'moment';
+
+function formata_data(data){
+    const momento = moment.unix(data.seconds)
+    return momento.format("DD/MM/YY")
+}
+
+defineProps({
+  carregamento: Object
+});
+</script>
