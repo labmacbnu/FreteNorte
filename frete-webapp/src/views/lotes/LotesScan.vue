9dea5d12926c34ef35a38b84c20533c2b4010516
Bossa <l.f.bossa@ufsc.br>
2024-03-18 14:43:45 -0300

    Adiciona botão para abrir/fechar leitor de QRCode em LotesScan.vue

--- frete-webapp/src/views/lotes/LotesScan.vue
+++ frete-webapp/src/views/lotes/LotesScan.vue
@@ -10,6 +10,7 @@ const router = useRouter()
 const lotes = useLotesStore()
 const mensagem = ref("Lote lido com sucesso!")
 
+const qrcode_show = ref(false)
 
 function update_url(new_url) {
   const prefix = "https://frete-norte-ufsc-blumenau.web.app/volumes/cod/"
@@ -31,8 +32,15 @@ function update_url(new_url) {
   <div class="row">
     <div class="col mb-3">
       <h1>Criando um lote</h1>
-      <p>Leia os QRCodes para adicionar os volumes nesse lote. Clique em Próximo para designar o caminhão que levará o lote.</p>
-      <QRCodeReader :autoreload="true" @decoded="update_url">
+      <p>Leia os QRCodes para adicionar os volumes nesse lote. Clique em Próximo para designar o caminhão que levará o
+        lote.</p>
+      <div>
+        <button @click="qrcode_show = !qrcode_show" class="btn"
+          :class="[qrcode_show ? 'btn-secondary' : 'btn-primary']"><i class="bi bi-camera"></i> {{ qrcode_show ?
+            "Fechar" :
+            "Abrir" }} leitor</button>
+      </div>
+      <QRCodeReader v-show="qrcode_show" :autoreload="true" @decoded="update_url">
         <p>{{ mensagem }}</p>
       </QRCodeReader>
     </div>
@@ -43,13 +51,24 @@ function update_url(new_url) {
       <ul class="list-group">
         <li class="list-group-item d-flex justify-content-between align-items-center" v-for="l in lotes.volumesCods">
           {{ l }}
-          <span @click="() => lotes.removeVolume(l)" class="badge text-bg-danger rounded-pill"><i class="bi bi-trash"></i></span>
+          <span @click="() => lotes.removeVolume(l)" class="badge text-bg-danger rounded-pill"><i
+              class="bi bi-trash"></i></span>
         </li>
       </ul>
     </div>
     <div class="col-lg-5 col-sm-12 text-center p-3">
-      <RouterLink :to="{name: 'lotes-carregamento'}" class="btn btn-primary">Próximo<i class="bi bi-caret-right"></i></RouterLink>
-
+      <div class="vstack gap-3">
+        <div class="p-2">
+          <RouterLink :to="{ name: 'lotes-carregamento' }" class="btn btn-lg btn-primary icon-link">
+            Carregando <i class="bi bi-box-arrow-in-up"></i><i class="bi bi-truck"></i>
+          </RouterLink>
+        </div>
+        <div class="p-2">
+          <RouterLink :to="{ name: 'lotes-descarregamento' }" class="btn  btn-lg btn-primary icon-link">
+            Descarregando <i class="bi bi-truck"></i><i class="bi bi-box-arrow-in-down"></i>
+          </RouterLink>
+        </div>
+      </div>
     </div>
   </div>
 </template>
