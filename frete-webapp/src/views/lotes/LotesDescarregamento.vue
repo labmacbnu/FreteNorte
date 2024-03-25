d3411eae215e9486721cca6bcf0815f29c4f6aef
Bossa <l.f.bossa@ufsc.br>
2024-03-25 17:17:53 -0300

    Adiciona seleção de sala e carregamento no registro de lote de descarregamento

--- frete-webapp/src/views/lotes/LotesDescarregamento.vue
+++ frete-webapp/src/views/lotes/LotesDescarregamento.vue
@@ -1,34 +1,49 @@
 <script setup>
-import { computed, ref, toValue } from 'vue';
+import { computed, ref, toValue, inject } from 'vue';
 import { useLotesStore } from '@/stores/lotes';
 import CarregamentoShow from '@/components/CarregamentoShow.vue';
+import SelectPlus from '@/components/SelectPlus.vue';
 import { useCollection } from 'vuefire'
 import { db } from '@/backend/index'
-import { collection } from 'firebase/firestore'
+import { collection, query, where, doc, setDoc } from 'firebase/firestore'
+import { useListaAmbientesNorteStore } from '@/stores/ambientes'
+
+const {globaluser, updateUser} = inject('globaluser')
 
 const lotes = useLotesStore()
  
-const carregamentos = useCollection(collection(db, 'carregamentos'))
+const carregamentos = useCollection(query(collection(db, 'carregamentos'), where("status", 'in', ["carregado"])))
 
 const carregamento_selecionado = ref(null)
 
-function registerLote(){
+const salas = useListaAmbientesNorteStore()
+ 
+const sala_selecionada = ref(null)
+
+
+
+function registerLoteDescarregamento(){
     const dados  = {
-        volumes: [...lotes.volumesCods],
-        caminhao: carregamento_selecionado.value
+        volumes: lotes.volumesCods.map(x => doc(db, 'volumes', x)),
+        carregamento: doc(db, 'carregamentos', carregamento_selecionado.value),
+        data_criado: new Date(),
+        responsavel: doc(db, 'usuarios', globaluser.value.email),
+        tipo: "Descarregamento",
+        sala: doc(db, 'ambientes-norte', sala_selecionada.value)
     }
     console.log("Lote registrado") 
+    const new_id = (dados.data_criado.getTime()).toString(36).toUpperCase() 
+    const new_docref = doc(db, 'lotes', new_id)
+    setDoc(new_docref, dados)
     console.log(dados)
 }
- 
-
 </script>
 <template>
 <div class="row">
     <div class="col-12">
-        <h1>Designar carregamento</h1>
+        <h1>Descarregando Caixas</h1>
     </div>
-    <div class="col-sm-12 col-md-6 mt-2">
+    <div class="col-sm-12 col-md-4 mt-2">
         <h4>Volumes</h4>
         <ul class="list-group">
         <li class="list-group-item d-flex justify-content-between align-items-center" v-for="l in lotes.volumesCods">
@@ -36,13 +51,31 @@ function registerLote(){
         </li>
       </ul>
     </div>
-    <div class="col-sm-12 col-md-6 mt-2">
-        <h4>Descarregando</h4>   
+    <div class="col-sm-12 col-md-4 mt-2"> 
+        <h4>Carregamento</h4>  
+        <p class="my-0 text-secondary">Selecione o carregamento.</p>
+            <div v-for="carregamento in carregamentos" :key="carregamento.id">
+                <CarregamentoShow 
+                @select:carregamento=" x => carregamento_selecionado = x" 
+                :carregamento="carregamento"
+                :selecionado="carregamento_selecionado === carregamento.id"
+                     />
+            </div> 
+    </div>
+    <div class="col-sm-12 col-md-4 mt-2">
+        <h4>Sala</h4>  
+        <p class="my-0 text-secondary">Selecione a sala que serão descarregados os volumes.</p>
+        <SelectPlus 
+            :options="salas.dados"
+            value="sala_selecionada"
+            @selected="x => sala_selecionada = x"
+            placeholder="Digite para pesquisar a sala"
+            />
     </div>
 
     <div class="col-12 text-center pt-3">
         <RouterLink :to="{name: 'lotes-scan'}" class="btn btn-primary"><i class="bi bi-caret-left"></i> Voltar</RouterLink> 
-        <button class="btn btn-success ms-3">Descarregar lote</button>
+        <button @click="registerLoteDescarregamento" class="btn btn-success ms-3">Descarregar lote</button>
     </div>
 </div> 
 </template>
