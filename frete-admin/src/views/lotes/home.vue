0c3070d7f4e08b8c4c19e2e8137315201f6dbf14
Bossa <l.f.bossa@ufsc.br>
2024-03-26 17:09:53 -0300

    Atualizações na página inicial de lotes

--- frete-admin/src/views/lotes/home.vue
+++ frete-admin/src/views/lotes/home.vue
@@ -5,21 +5,49 @@
 
         </div>
         <div class="col-4 text-end">
-            <RouterLink :to="{name: 'lotes-add'}" class="btn btn-primary"><i class="bi bi-plus"></i>Adicionar carregamento</RouterLink>
+            <RouterLink :to="{ name: 'lotes-add' }" class="btn btn-primary"><i class="bi bi-plus"></i>Adicionar
+                carregamento</RouterLink>
         </div>
     </div>
     <div class="row">
-        <div v-for="carregamento in carregamentos" :key="carregamento.id" class="col-4"> 
+        <!-- <div v-for="carregamento in carregamentos" :key="carregamento.id" class="col-sm-6 col-md-4"> 
             <CarregamentoShow  :carregamento="carregamento" />
+        </div> -->
+        <div class="col">
+            <CarregamentoTable :carregamentos="carregamentos" />
         </div>
-    </div>
+    </div> 
 </template>
 <script setup>
+import { ref, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 import { useCollection } from 'vuefire';
 import { db } from '@/backend';
-import { collection } from 'firebase/firestore';
-import CarregamentoShow  from '@/components/CarregamentoShow.vue';
+import { query, collection, where, doc, getAggregateFromServer, sum } from 'firebase/firestore';  
+import CarregamentoShow from '@/components/CarregamentoShow.vue';
+import { getLotesFromCarregamento } from '@/stores/lotes'
 
-const carregamentos = useCollection(collection(db, 'carregamentos'));
+
+import CarregamentoTable from '@/components/CarregamentoTable.vue';
+import moment from 'moment';
+
+function formata_data(data) {
+    const momento = moment.unix(data.seconds)
+    return momento.format("DD/MM/YY")
+}
+
+
+const {data: carregamentos, pending} = useCollection(collection(db, 'carregamentos'));
+
+// Código para criar os índices
+// const lotesRef = collection(db, "lotes")
+// const carregamentoRef = doc(db, 'carregamentos', 'C0002')
+// const pesquisa = query(lotesRef, where('carregamento', '==', carregamentoRef), where('tipo', '==', 'Descarregamento'))
+// const mylotes = useCollection(pesquisa, {maxRefDepth: 1})  
+
+// const k = ref(-1)
+// onMounted(async () => {
+//     const result = await getAggregateFromServer(pesquisa, {k: sum('n_volumes')})
+//     k.value = result.data().k 
+// })
 </script>
