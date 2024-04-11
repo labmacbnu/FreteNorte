84cdcb8f620374930a566e08b2dfda80cfeaf99f
Bossa <l.f.bossa@ufsc.br>
2024-04-11 19:45:24 -0300

    Adiciona validação

--- frete-admin/src/views/lotes/add.vue
+++ frete-admin/src/views/lotes/add.vue
@@ -14,13 +14,13 @@
                 <div class="row mb-2">
                     <label for="dataSaida" class="col-sm-2 col-form-label">Data de saída</label>
                     <div class="col-sm-10">
-                        <input v-model="data_saida" type="datetime-local" class="form-control" id="dataSaida">
+                        <input :class="{'border-danger': !dados_validos.data_saida}" v-model="data_saida" type="datetime-local" class="form-control" id="dataSaida">
                     </div>
                 </div>
                 <div class="row mb-2">
                     <label for="status" class="col-sm-2 col-form-label">Status</label>
                     <div class="col-sm-10">
-                        <select disabled v-model="novo_carregamento.status" class="form-select" id="status">
+                        <select  disabled v-model="novo_carregamento.status" class="form-select" id="status">
                             <option value="agendado">agendado</option>
                         </select>
                     </div>
@@ -28,7 +28,7 @@
                 <div class="row mb-2">
                     <label for="saida" class="col-sm-2 col-form-label">Local de saída</label>
                     <div class="col-sm-10">
-                        <select v-model="novo_carregamento.saida" class="form-select" id="saida">
+                        <select :class="{'border-danger': !dados_validos.saida}"  v-model="novo_carregamento.saida" class="form-select" id="saida">
                             <option value="bloco A">bloco A</option>
                             <option value="bloco B">bloco B</option>
                             <option value="bloco C">bloco C</option>
@@ -38,7 +38,7 @@
                 <div class="row mb-2">
                     <label for="empresa" class="col-sm-2 col-form-label">Empresa</label>
                     <div class="col-sm-10">
-                        <select v-model="empresa_id" class="form-select" id="empresa" >
+                        <select :class="{'border-danger': !dados_validos.empresa}"  v-model="empresa_id" class="form-select" id="empresa" >
                             <option :value="null" disabled selected>Selecione uma empresa</option>
                             <option v-for="e in empresas" :key="e.id" :value="e.id">{{ e.nome }}</option>
                         </select>
@@ -48,7 +48,7 @@
                 <div class="row mb-2">
                     <label for="caminhao" class="col-sm-2 col-form-label">Caminhão</label>
                     <div class="col-sm-10">
-                        <select v-model="caminhao_id" class="form-select" id="caminhao">
+                        <select :class="{'border-danger': !dados_validos.caminhao}"  v-model="caminhao_id" class="form-select" id="caminhao">
                             <option :value="null" disabled selected>Selecione uma empresa primeiro</option>
                             <option v-for="c in caminhoes_filtrados" :key="c.id" :value="c.id">{{ c.placa }}</option>
                         </select>
@@ -102,6 +102,13 @@ const novo_carregamento = reactive({
     saida: null
 })
 
+const dados_validos = reactive({
+    data_saida: true,
+    empresa: true,
+    caminhao: true,
+    saida: true
+})
+
 // data saída 
 const data_saida = ref(null)
 
@@ -112,6 +119,18 @@ watch(data_saida, (nv, ov) => {
 const router = useRouter()
 
 async function saveCarregamento() {
+    if (!validarEntradas()) {
+        console.log('Dados inválidos')
+        setTimeout(() => {
+            Object.assign(dados_validos, {
+                data_saida: true,
+                empresa: true,
+                caminhao: true,
+                saida: true
+            })
+        }, 2000)
+        return
+    }
     const valores = toValue(novo_carregamento) 
     console.log(valores)
     const newdoc = doc(db, 'carregamentos', valores.id)
@@ -125,6 +144,14 @@ async function idUltimoCarregamento() {
     const {data: ultimo, promise} = useCollection(pesquisa, { maxRefDepth: 0 }); 
     await promise.value
     return ultimo.value[0].id
+}
+
+function validarEntradas(){
+    dados_validos.data_saida = novo_carregamento.data_saida !== null
+    dados_validos.empresa = novo_carregamento.empresa !== null
+    dados_validos.caminhao = novo_carregamento.caminhao !== null
+    dados_validos.saida = novo_carregamento.saida !== null
+    return Object.values(dados_validos).every(v => v)
 }
 
 function hexToDecimal(hexString) {
