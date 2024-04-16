cbb4d8f9bab2146aaf608b4a5815e5c37358e3bb
Bossa <l.f.bossa@ufsc.br>
2024-04-16 17:33:18 -0300

    inserindo dados no emlator

---
+++ frete-admin/scripts/testing.mjs
@@ -0,0 +1,16 @@
+import { getFirestore, doc, collection, setDoc, connectFirestoreEmulator } from 'firebase/firestore'
+import { firebaseApp } from './firebaseConfig.mjs'
+const db = getFirestore()
+connectFirestoreEmulator(db, 'localhost', 8080)
+
+import fs  from 'fs'
+const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+
+const VolumesLEMA = loadJSON('./volumes-lema.json'); 
+
+
+
+Object.entries(VolumesLEMA).forEach(async ([key, value]) => {
+    const docRef = doc(db, 'volumes', key)
+    await setDoc(docRef, value)
+})
