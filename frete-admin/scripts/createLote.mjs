085dea6329f35d93c7d772d9753fb1db1b01fce8
Bossa <l.f.bossa@ufsc.br>
2024-04-17 17:07:39 -0300

    testing descarregamento

---
+++ frete-admin/scripts/createLote.mjs
@@ -0,0 +1,26 @@
+import { getFirestore, doc, collection, setDoc, connectFirestoreEmulator } from 'firebase/firestore' 
+import { firebaseApp } from './firebaseConfig.mjs'
+const db = getFirestore(firebaseApp)
+connectFirestoreEmulator(db, 'localhost', 8080)
+
+const bossa_ref = doc(db, 'usuarios', 'l.f.bossa@ufsc.br')
+const carregamento_ref = doc(db, 'carreganebtis', 'C0003')
+const volumes_cods = ['V363741', 'V353341I', 'B1230028', 'V353341E']
+const volumes_refs = volumes_cods.map((cod) => doc(db, 'volumes', cod))
+const data_criacao = new Date()
+const tipo = "Descarregamento"
+const n_volumes = volumes_refs.length
+
+const data = {
+    responsavel: bossa_ref,
+    carregamento: carregamento_ref,
+    volumes: volumes_refs,
+    data_criacao,
+    tipo,
+    n_volumes,
+    sala: doc(db, 'ambientes-norte', 'A102')
+}
+
+const new_id = (data_criacao.getTime()).toString(36).toUpperCase() 
+const docRef = doc(db, 'lotes', new_id)
+await setDoc(docRef, data)
