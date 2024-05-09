56b89250fd6a7cfcf50af41bb7e116db22683aae
Bossa <l.f.bossa@ufsc.br>
2024-05-09 16:05:27 -0300

    adiciona um filtro para impressão

--- frete-webapp/src/views/volumes/ambiente-print-etiqueta14.vue
+++ frete-webapp/src/views/volumes/ambiente-print-etiqueta14.vue
@@ -5,8 +5,8 @@ import { useRoute } from 'vue-router';
 
 import { db } from '@/backend/index.js';
 import { collection, where, doc, orderBy, query } from 'firebase/firestore';
-import { useCollection } from 'vuefire';
-import { RouterLink } from 'vue-router';
+import { useCollection } from 'vuefire'; 
+import { computed, reactive } from 'vue';
 
 const route = useRoute()
 
@@ -14,8 +14,20 @@ const thisAmbienteRef = doc(db, "ambientes", route.params.ambiente)
 const q = query(collection(db, "volumes"), where('origem', '==', thisAmbienteRef), where('deleted', '==', false), orderBy("data_criacao", "desc"))
 const { data: volumes, pending, promise } = useCollection(q)
 
+const selecionados = reactive({})
 
 
+promise.value.then(() => {
+  for (let volume of volumes.value) {
+    if (!selecionados[volume.categoria]) {
+      selecionados[volume.categoria] = true
+    }
+  }
+})
+
+const volumes_filtrados = computed(() => {
+  return volumes.value.filter(volume => selecionados[volume.categoria])
+})
 
 function print() {
   window.print()
@@ -29,12 +41,21 @@ function print() {
       <h1>Etiquetas dos volumes do ambiente {{ route.params.ambiente }}</h1>
     </div>
     <div class="col-12 d-print-none">
-      <p @click="print" role="button" class="d-print-none form-text">Essa página foi otimizada para impressão. Clique
+      <p @click="print" role="button" class="d-print-none form-text">Essa página foi otimizada para impressão de etiquetas 14. Clique
         aqui para imprimir <i class="bi bi-printer"></i>.</p>
+    </div>
+    <div class="col"> 
+      <p class="mb-1">Selecione as categorias a exibir</p>
+      <template v-for="categoria in Object.keys(selecionados)">
+        <div class="form-check form-switch form-check-inline">
+          <input class="form-check-input" type="checkbox" :id="'check' + categoria" v-model="selecionados[categoria]">
+          <label class="form-check-label" :for="'check' + categoria">{{categoria}}</label>
+        </div> 
+      </template> 
     </div>
   </div>
   <div id="etiquetacontainer">
-    <EtiquetaVolume v-for="volume in volumes" :key="volume.codigo" :volume="volume"></EtiquetaVolume>
+    <EtiquetaVolume v-for="volume in volumes_filtrados" :key="volume.codigo" :volume="volume"></EtiquetaVolume>
   </div>
 </template>
 <style>
