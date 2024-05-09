be7655edf1e76abc85b4a05af8a89b26016cc665
Bossa <l.f.bossa@ufsc.br>
2024-05-09 15:42:02 -0300

    QRCode pequeno

---
+++ frete-webapp/src/components/QRCodeS.vue
@@ -0,0 +1,28 @@
+<script setup>
+import VueQrcode from 'vue-qrcode';
+const baseurl = "https://frete-norte-ufsc-blumenau.web.app"
+defineProps({
+    path: String
+})
+</script>
+<template> 
+<div class="qrcode">
+<VueQrcode :color="{
+        dark: '#000',
+        light: '#FFF'
+      }" 
+      :value="baseurl + path"
+      :size="200" type="image/png"
+      errorCorrectionLevel="M"
+      :version="5"
+      :margin="1"
+      :scale="3"
+      ></VueQrcode>
+</div>
+</template>
+<style>
+.qrcode {
+  width: fit-content;
+  height: fit-content;
+} 
+</style>
