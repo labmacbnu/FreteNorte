188a79e8e42413642188b89c7e7a0e3653ae2e89
Bossa <l.f.bossa@ufsc.br>
2024-05-09 16:11:08 -0300

    feat: Adicionar filtro de categorias para impressão de volumes

--- frete-webapp/src/views/volumes/ambiente-print-long.vue
+++ frete-webapp/src/views/volumes/ambiente-print-long.vue
@@ -1,4 +1,4 @@
-<script setup> 
+<script setup>
 import ConteudoVolume from '@/components/ConteudoVolume.vue';
 
 import { useRoute } from 'vue-router';
@@ -6,50 +6,73 @@ import { useRoute } from 'vue-router';
 import { db } from '@/backend/index.js';
 import { collection, where, doc, orderBy, query } from 'firebase/firestore';
 import { useCollection } from 'vuefire';
-import { RouterLink } from 'vue-router';
- 
+import { computed, reactive } from 'vue';
+
 
 const route = useRoute()
 
 const thisAmbienteRef = doc(db, "ambientes", route.params.ambiente)
-const q = query(collection(db, "volumes"), where('origem', '==', thisAmbienteRef), where('deleted', '==', false),   orderBy("data_criacao", "desc"))
-const {data: volumes, pending, promise} = useCollection(q)
+const q = query(collection(db, "volumes"), where('origem', '==', thisAmbienteRef), where('deleted', '==', false), orderBy("data_criacao", "desc"))
+const { data: volumes, pending, promise } = useCollection(q)
 
 function print() {
   window.print()
 }
+
+const selecionados = reactive({})
+
+promise.value.then(() => {
+  for (let volume of volumes.value) {
+    if (!selecionados[volume.categoria]) {
+      selecionados[volume.categoria] = true
+    }
+  }
+})
+
+const volumes_filtrados = computed(() => {
+  return volumes.value.filter(volume => selecionados[volume.categoria])
+})
+
 </script>
 
-<template> 
-  <div class="row mb-3 align-items-end">
-    <div class="col-12 d-print-none">
+<template>
+  <div class="row mb-3 d-print-none align-items-end">
+    <div class="col-12 ">
       <h1>Conteúdos dos volumes do ambiente {{ route.params.ambiente }}</h1>
-    </div> 
+    </div>
     <div class="col-12 d-print-none">
-      <p @click="print" role="button" class="d-print-none form-text">Essa página foi otimizada para impressão. Clique aqui para imprimir <i class="bi bi-printer"></i>.</p>
-</div>
+      <p @click="print" role="button" class="d-print-none form-text">Essa página foi otimizada para impressão. Clique
+        aqui para imprimir <i class="bi bi-printer"></i>.</p>
+    </div>
+
+    <div class="col"> 
+      <p class="mb-1">Selecione as categorias a exibir</p>
+      <template v-for="categoria in Object.keys(selecionados)">
+        <div class="form-check form-switch form-check-inline">
+          <input class="form-check-input" type="checkbox" :id="'check' + categoria" v-model="selecionados[categoria]">
+          <label class="form-check-label" :for="'check' + categoria">{{categoria}}</label>
+        </div> 
+      </template> 
+    </div>
   </div>
-  <template v-if="!pending" v-for="volume in volumes" :key="volume.codigo">   
-      <ConteudoVolume :volume="volume" />
+  <template v-if="!pending" v-for="volume in volumes_filtrados" :key="volume.codigo">
+    <ConteudoVolume :volume="volume" />
   </template>
 
- 
+
 </template>
 <style>
-
 .listinhadelete {
   max-height: 20vh;
 }
+
 .text-elipse {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 }
+
 .p-lista-nao-volumados {
   width: 5em;
 }
-
-
-
 </style>
- 
--- frete-webapp/src/views/volumes/ambiente-print-short.vue
+++ frete-webapp/src/views/volumes/ambiente-print-short.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {  inject, ref } from 'vue'; 
-import { useUserPermissionsStore } from '@/stores/user'; 
-import {  registra_volume, apaga_volume, useVolumesEmailStore } from '@/stores/volumes'; 
+import { inject, ref } from 'vue';
+import { useUserPermissionsStore } from '@/stores/user';
+import { registra_volume, apaga_volume, useVolumesEmailStore } from '@/stores/volumes';
 import EtiquetaVolume from '@/components/EtiquetaVolume.vue';
 
 import { useRoute } from 'vue-router';
@@ -9,55 +9,30 @@ import { useRoute } from 'vue-router';
 import { db } from '@/backend/index.js';
 import { collection, where, doc, orderBy, query } from 'firebase/firestore';
 import { useCollection } from 'vuefire';
-import { RouterLink } from 'vue-router';
+import { reactive, computed } from 'vue';
 
 const { globaluser, updateUser } = inject("globaluser")
-const permissoes = useUserPermissionsStore() 
+const permissoes = useUserPermissionsStore()
 
 const route = useRoute()
 
 const thisAmbienteRef = doc(db, "ambientes", route.params.ambiente)
-const q = query(collection(db, "volumes"), where('origem', '==', thisAmbienteRef), where('deleted', '==', false),   orderBy("data_criacao", "desc"))
-const {data: volumes, pending, promise} = useCollection(q)
+const q = query(collection(db, "volumes"), where('origem', '==', thisAmbienteRef), where('deleted', '==', false), orderBy("data_criacao", "desc"))
+const { data: volumes, pending, promise } = useCollection(q)
 
-function click_row(i) {
-  document.getElementById("check" + i).click()
-}
+const selecionados = reactive({})
 
- 
-
-
-async function salvar_volume() {
-  const volume = {
-    categoria: "NULL",
-    responsavel: globaluser.value.email,
-    items: lista_items.value
-  }
-  console.log(volume)
-  const uptime = registra_volume(volume)
-  if (uptime) {
-    for (let item of lista_items.value) {
-      var itemRef = all_items_ordered.value.find(x => x.key == item)
-      itemRef.volumado = true
+promise.value.then(() => {
+  for (let volume of volumes.value) {
+    if (!selecionados[volume.categoria]) {
+      selecionados[volume.categoria] = true
     }
-    lista_items.value = []
-
-    return true
-  } else {
-    return false
   }
-}
- 
+})
 
-async function soft_apaga_volume(codigo) { 
-  console.log(`Apagando volume ${codigo}`)
-  const volume_registry = volumes.value.find(x => x.codigo == codigo)
-  const items_do_volume = volume_registry.items 
-  const uptime = apaga_volume(codigo)
-  return true
-}
-
-const soft_volume_modal_ref = ref(null)
+const volumes_filtrados = computed(() => {
+  return volumes.value.filter(volume => selecionados[volume.categoria])
+})
 
 function print() {
   window.print()
@@ -65,38 +40,45 @@ function print() {
 
 </script>
 
-<template> 
-  <div class="row mb-3 align-items-end">
-    <div class="col-12 d-print-none">
+<template>
+  <div class="row mb-3 d-print-none align-items-end">
+    <div class="col-12 ">
       <h1>Etiquetas dos volumes do ambiente {{ route.params.ambiente }}</h1>
-    </div> 
+    </div>
     <div class="col-12 d-print-none">
-      <p @click="print" role="button" class="d-print-none form-text">Essa página foi otimizada para impressão. Clique aqui para imprimir <i class="bi bi-printer"></i>.</p>
+      <p @click="print" role="button" class="d-print-none form-text">Essa página foi otimizada para impressão. Clique
+        aqui para imprimir <i class="bi bi-printer"></i>.</p>
+    </div>
+    <div class="col"> 
+      <p class="mb-1">Selecione as categorias a exibir</p>
+      <template v-for="categoria in Object.keys(selecionados)">
+        <div class="form-check form-switch form-check-inline">
+          <input class="form-check-input" type="checkbox" :id="'check' + categoria" v-model="selecionados[categoria]">
+          <label class="form-check-label" :for="'check' + categoria">{{categoria}}</label>
+        </div> 
+      </template> 
     </div>
   </div>
   <div class="row g-0" v-if="!pending">
-    <div class="col p-0 " v-for="volume in volumes" :key="volume.codigo">
+    <div class="col p-0 " v-for="volume in volumes_filtrados" :key="volume.codigo">
       <EtiquetaVolume :volume="volume" />
     </div>
   </div>
 
- 
+
 </template>
 <style>
-
 .listinhadelete {
   max-height: 20vh;
 }
+
 .text-elipse {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 }
+
 .p-lista-nao-volumados {
   width: 5em;
 }
-
-
-
 </style>
- 
