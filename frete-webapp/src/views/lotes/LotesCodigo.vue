b8a70c76f994a70d98f0b2bc89ea1b22425182ad
Bossa <l.f.bossa@ufsc.br>
2024-05-23 20:03:38 -0300

    feat: Atualiza componente LotesCodigo.vue para exibir nome do lote e volumes relacionados

--- frete-webapp/src/views/lotes/LotesCodigo.vue
+++ frete-webapp/src/views/lotes/LotesCodigo.vue
@@ -1,20 +1,21 @@
 <template>
-<div class="row">
-    <div class="col-12">
-        <div class="hstack justify-content-between">
-            <h1>Lote {{ loteCodigo }}</h1>
-            <RouterLink class="btn btn-primary" :to="{ name: 'lotes-editar', params: {codigo: loteCodigo} }">Editar</RouterLink>
+    <div class="row">
+        <div class="col-12">
+            <div class="hstack justify-content-between">
+                <h1>Lote {{ loteCodigo }}</h1>
+                <RouterLink class="btn btn-primary" :to="{ name: 'lotes-editar', params: { codigo: loteCodigo } }">Editar
+                </RouterLink>
+            </div>
+
+            <p class="fs-3" v-if="!pending"> {{ lote.nome }}</p>
+
         </div>
-        
-        <p class="fs-3"  v-if="!pending"> {{ lote.nome }}</p>
+        <div class="col-12">
+            <h3>Volumes desse lote</h3>
+            <VolumesTable v-if="!pending" :volumes="lote.volumes"></VolumesTable>
+        </div>
 
     </div>
-    <div class="col-12">
-<h3>Volumes desse lote</h3>
-<VolumesTable v-if="!pending" :volumes="lote.volumes"></VolumesTable>
-    </div>
-
-</div>
 
 </template>
 <script setup>
@@ -23,12 +24,12 @@ import { useRoute, useRouter } from 'vue-router'
 import { db } from '@/backend/index'
 import { doc, getDoc } from 'firebase/firestore'
 import { useDocument } from 'vuefire'
-import VolumesTable  from '@/components/SimpleVolumesTable.vue'
+import VolumesTable from '@/components/SimpleVolumesTable.vue'
 
 const router = useRouter()
 const route = useRoute()
 const loteCodigo = route.params.codigo
 
-const {data: lote, pending, promise} = useDocument(doc(db, 'lotes', loteCodigo))
+const { data: lote, pending, promise } = useDocument(doc(db, 'lotes', loteCodigo), { wait: true })
 
 </script>
