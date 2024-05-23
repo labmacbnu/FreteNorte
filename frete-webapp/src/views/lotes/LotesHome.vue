00378e4db056afa856ea8d471795a76e8a7fc277
Bossa <l.f.bossa@ufsc.br>
2024-05-23 17:45:39 -0300

    home dos lotes

---
+++ frete-webapp/src/views/lotes/LotesHome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <div class="hstack justify-content-between">
+                <h1>Lotes</h1>
+                <RouterLink class="btn btn-primary" :to="{name: 'lotes-scan'}">Criar lote</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
