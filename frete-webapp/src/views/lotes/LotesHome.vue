e16632053296367c6acc7bbbfbb74ca9bbf1265c
Bossa <l.f.bossa@ufsc.br>
2024-05-24 09:45:48 -0300

    link para o admin

--- frete-webapp/src/views/lotes/LotesHome.vue
+++ frete-webapp/src/views/lotes/LotesHome.vue
@@ -3,9 +3,14 @@
         <div class="col-12">
             <div class="hstack justify-content-between">
                 <h1>Lotes</h1>
-                <RouterLink class="btn btn-primary" :to="{ name: 'lotes-scan' }">Criar lote</RouterLink>
+                <RouterLink class="btn btn-lg btn-primary" :to="{ name: 'lotes-scan' }"><i class="bi-plus bi"></i> Criar
+                    lote</RouterLink>
             </div>
         </div>
+        <div class="col-12 mb-3">
+            <a class="link-offset-2 fs-4" :href="linkAdmin">Ir para o Admin <i class="bi-link-45deg bi"></i></a>
+        </div>
+
         <div class="col-12">
             <h4>Lotes sem carregamento</h4>
 
@@ -16,7 +21,8 @@
                 <li class="list-group-item d-flex justify-content-between align-items-center"
                     v-for="lote in lotesNaoCarregados">
                     <div class="ms-2 me-auto">
-                        <RouterLink :to="{name: 'lotes-codigo', params: {codigo: lote.id}}" class="d-block text-secondary">{{ lote.id }}</RouterLink>
+                        <RouterLink :to="{ name: 'lotes-codigo', params: { codigo: lote.id } }"
+                            class="d-block text-secondary">{{ lote.id }}</RouterLink>
                         {{ lote.nome }}
                     </div>
                     <span class="badge bg-primary rounded-pill">{{ lote.n_volumes }} volume</span>
@@ -30,10 +36,20 @@
 import { db } from '@/backend/index'
 import { collection, query, where, doc } from 'firebase/firestore'
 import { useCollection } from 'vuefire';
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const carregamentoZero = doc(db, 'carregamentos', "C0000")
 const { data: lotesNaoCarregados, pending } = useCollection(query(collection(db, 'lotes'), where("carregamento", "==", carregamentoZero)))
 
+const linkAdmin = computed(() => {
+    const hostname = window.location.hostname
+    if (hostname == "localhost") {
+        return window.location.origin + "/login"
+    } else {
+        const href = window.location.href
+        const adminhref = href.replace(hostname, "admin-" + hostname)
+        return adminhref
+    } 
+})
 
 </script>
