8302269684f90e96463ab8b02aacd1113cad53de
Bossa <l.f.bossa@ufsc.br>
2024-05-24 13:38:00 -0300

    solução simples: vai pro sislog normal

---
+++ frete-admin/src/views/lotes/LotesHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <a class="link-offset-2 fs-4" :href="linkAdmin">
+    Ir para Sislog<i class="bi-link-45deg bi"></i>
+    </a>
+</template>
+<script setup>
+import { computed } from 'vue';
+const linkAdmin = computed(() => {
+    const hostname = window.location.hostname
+    if (hostname == "localhost") {
+        return window.location.origin + "/login"
+    } else {
+        const href = window.location.href
+        const adminhref = href.replace("admin-frete", "frete")
+        return adminhref
+    }
+})
+</script>
