c717fa2bcf5142f3208cb597cdd6725b1d99b746
Bossa <l.f.bossa@ufsc.br>
2024-05-28 18:11:41 -0300

    feat: Adiciona visualização de lotes carregados em LotesHome.vue

--- frete-webapp/src/views/lotes/LotesHome.vue
+++ frete-webapp/src/views/lotes/LotesHome.vue
@@ -29,17 +29,42 @@
                 </li>
             </ul>
         </div>
+        <div class="col-12">
+            <h4 class="mt-3">Lotes carregados</h4>
+            <ul v-if="pendingCarregados" class="list-group  ">
+                <li class="list-group-item">Carregando...</li>
+            </ul>
+            <ul v-else class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center"
+                    v-for="lote in lotesCarregados">
+                    <div class="ms-2">
+                        <RouterLink :to="{ name: 'lotes-codigo', params: { codigo: lote.id } }"
+                            class="d-block text-secondary">{{ lote.id }}</RouterLink>
+                        <p class="my-1">{{ lote.nome }}</p>
+                        <p class="mb-1">
+                            <a :href="linkCarregamento(lote.carregamento.id)" class="link">
+                                {{ lote.carregamento.id }}
+                            </a>
+                        </p>
+                    </div>
+                    <span class="badge bg-primary rounded-pill">{{ lote.n_volumes }} volume</span>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script setup>
 
 import { db } from '@/backend/index'
-import { collection, query, where, doc } from 'firebase/firestore'
+import { collection, query, where, doc, limit, orderBy } from 'firebase/firestore'
 import { useCollection } from 'vuefire';
 import { computed, ref } from 'vue'
 
 const carregamentoZero = doc(db, 'carregamentos', "C0000")
 const { data: lotesNaoCarregados, pending } = useCollection(query(collection(db, 'lotes'), where("carregamento", "==", carregamentoZero)))
+
+const { data: lotesCarregados, pending: pendingCarregados } = useCollection(query(collection(db, 'lotes'), where("carregamento", "!=", carregamentoZero), limit(10)))
+
 
 const linkAdmin = computed(() => {
     const hostname = window.location.hostname
@@ -49,7 +74,13 @@ const linkAdmin = computed(() => {
         const href = window.location.href
         const adminhref = href.replace(hostname, "admin-" + hostname)
         return adminhref
-    } 
+    }
 })
+
+function linkCarregamento(carregamentoId){
+    const origin = window.location.origin
+    const neworigin = origin.replace("frete-", "admin-frete-")
+    return neworigin + `/carregamentos/cod/${carregamentoId}`
+}
 
 </script>
