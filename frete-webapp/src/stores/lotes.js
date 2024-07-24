749b0f9e02d5c5284631d926013a057cbb076832
Bossa <l.f.bossa@ufsc.br>
2024-07-24 13:06:38 -0300

    feat: Add localStorage support for volumesCods in useLotesStore

--- frete-webapp/src/stores/lotes.js
+++ frete-webapp/src/stores/lotes.js
@@ -1,9 +1,18 @@
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { defineStore } from 'pinia'
 
 
 export const useLotesStore = defineStore('lotes', ()=>{
+
     const volumesCods = ref([])
+    if(localStorage.getItem('volumesCods')){
+        volumesCods.value = JSON.parse(localStorage.getItem('volumesCods'))
+    }
+
+    watch(() => volumesCods.value, (newValue, oldValue)=>{ 
+        localStorage.setItem('volumesCods', JSON.stringify(newValue))
+    }, {deep: true})  
+
     function addVolume(volume){
         volumesCods.value.push(volume)
     }
